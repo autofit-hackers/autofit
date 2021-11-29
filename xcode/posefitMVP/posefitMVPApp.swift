8df39894f4643087f43c7c803d8bd9bd4f39a52f
s-endo17 <yattsa0902@gmail.com>
2021-11-29 09:58:04 +0900

    chore: xcode project init

---
+++ xcode/posefitMVP/posefitMVPApp.swift
@@ -0,0 +1,20 @@
+//
+//  posefitMVPApp.swift
+//  posefitMVP
+//
+//  Created by Yusuke Kondo on 2021/11/29.
+//
+
+import SwiftUI
+
+@main
+struct posefitMVPApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ xcode/posefitMVPTests/posefitMVPTests.swift
@@ -0,0 +1,33 @@
+//
+//  posefitMVPTests.swift
+//  posefitMVPTests
+//
+//  Created by Yusuke Kondo on 2021/11/29.
+//
+
+import XCTest
+@testable import posefitMVP
+
+class posefitMVPTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
