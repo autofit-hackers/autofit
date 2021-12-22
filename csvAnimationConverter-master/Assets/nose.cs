c342a306da4822f17ba0cfa9be3ccb5aa47482d8
s-endo17 <yattsa0902@gmail.com>
2021-12-22 22:09:19 +0900

    feat: Keyがstringのjsonで可視化

--- csvAnimationConverter-master/Assets/nose.cs
+++ csvAnimationConverter-master/Assets/nose.cs
@@ -27,14 +27,12 @@ class nose : MonoBehaviour
     public string filename;
     public int startFrame = 1;
     public int endFrame = 10;
+    public float sphereScale = 0.1f;
 
+    public Transform spherePrefab;
     public List<Dictionary<string, Joint>> deserializedFrames = new List<Dictionary<string, Joint>>();
-    // public List<Transform> myTransforms = new List<Transform>(33);
-    public Dictionary<string,Transform> jointTransforms = new Dictionary<string,Transform>();
+    public Dictionary<string,GameObject> jointGameObjects = new Dictionary<string,GameObject>();
 
-    // public List<int> targetJointIndices2 = new List<int>() {0, 11, 12, 13, 14, 15, 16, 23, 24, 25, 26, 27, 28};
-    // public List<Dictionary<int, Joint>> deserializedFrames2 = new List<Dictionary<int, Joint>>();
-    // public List<Transform> myTransforms2 = new List<Transform>(32);
     public Dictionary<string,Vector3> lastFramePoses = new Dictionary<string,Vector3>();
     public float timeOut = 0.05f;
     private float timeElapsed = 0.0f;
@@ -66,6 +64,13 @@ class nose : MonoBehaviour
         Debug.Log("numOfJoints");
         Debug.Log(numOfJoints);
         
+        // Initialize joint objects
+        // NOTE: get joint names from 0 frame of deserializedFrames
+        foreach (var jointName in deserializedFrames[0].Keys) {
+            jointGameObjects[jointName] = Instantiate<GameObject>(spherePrefab.gameObject, Vector3.zero, Quaternion.identity);
+            jointGameObjects[jointName].transform.localScale = new Vector3(sphereScale, sphereScale, sphereScale);
+        }
+
         // foreach (var cylinderPrefab in cylinderPrefabs)
         // {
         //     InstantiateCylinder(cylinderPrefab, new Vector3(-1, 0, 0), new Vector3(1, 0, 0));
@@ -88,7 +93,7 @@ class nose : MonoBehaviour
                 Joint joint = jointItem.Value;
                 // Joint joint = deserializedFrame[jointIdx];
                 // XXX: ?
-                Transform jointTransform = jointTransforms[jointName];
+                Transform jointTransform = jointGameObjects[jointName].transform;
                 // Transform myTransform = myTransforms[jointIdx];
 
                 if (frameCount == 0){
@@ -96,7 +101,7 @@ class nose : MonoBehaviour
                     lastFramePoses.Add(jointName,jointTransform.position);
                 }
                 
-                // Vector3 pos = jointTransform.position;
+                // Vector3 pos = jointGameObject.position;
                 Vector3 pos = jointTransform.position;
 
                 //Debug.Log(joint.X); // , joint.Y, joint.Z);
@@ -106,12 +111,12 @@ class nose : MonoBehaviour
 
                 // LPF
                 pos = lastFramePoses[jointName] * lpf_rate + pos * (1 - lpf_rate);
-                jointTransform.position = pos; // 各座標に直接値を代入することはできない
+                jointGameObjects[jointName].transform.position = pos; // 各座標に直接値を代入することはできない
                 lastFramePoses[jointName] = pos;
 
                 // foreach (var cylinderPrefab in cylinderPrefabs)
                 // {
-                //     UpdateCylinderPosition(cylinderPrefab, jointTransforms[i].position, jointTransforms[i+1].position);
+                //     UpdateCylinderPosition(cylinderPrefab, jointGameObjects[i].position, jointGameObjects[i+1].position);
                 // }
             }
 
@@ -138,6 +143,7 @@ class nose : MonoBehaviour
         cylinder = Instantiate<GameObject>(cylinderPrefab.gameObject, Vector3.zero, Quaternion.identity);
         UpdateCylinderPosition(cylinder, beginPoint, endPoint);
     }
+
 
     private void UpdateCylinderPosition(GameObject cylinder, Vector3 beginPoint, Vector3 endPoint)
     {
