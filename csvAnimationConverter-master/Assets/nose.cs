8e9f6357bb793ff713ed1760750083ec46fd11f3
s-endo17 <yattsa0902@gmail.com>
2021-12-24 01:51:50 +0900

    feat: 関節間にboneを表示（boneはDictionary形式で保持）

--- csvAnimationConverter-master/Assets/nose.cs
+++ csvAnimationConverter-master/Assets/nose.cs
@@ -21,9 +21,9 @@ class nose : MonoBehaviour
 {
     // Start is called before the first frame update
     private static int numOfJoints = 33;
-    public int frameCount = 0;
+    private int frameCount = 0;
     public int frameCountMax;
-    public int loopCount = 0;
+    private int loopCount = 0;
     public string filename;
     public int startFrame = 1;
     public int endFrame = 10;
@@ -79,49 +79,43 @@ class nose : MonoBehaviour
         
         // Initialize bone names
         boneEdgeNames.Add("Shoulders", ("LeftShoulder", "RightShoulder"));
-        // boneEdgeNames.Add("LeftUpperArm", ("LeftShoulder", "LeftElbow"));
-        // boneEdgeNames.Add("LeftForeArm", ("LeftElbow", "LeftWrist"));
+        boneEdgeNames.Add("LeftUpperArm", ("LeftShoulder", "LeftElbow"));
+        boneEdgeNames.Add("RightUpperArm", ("RightShoulder", "RightElbow"));
+        boneEdgeNames.Add("LeftForeArm", ("LeftElbow", "LeftWrist"));
+        boneEdgeNames.Add("RightForeArm", ("RightElbow", "RightWrist"));
+        boneEdgeNames.Add("LeftFlank", ("LeftShoulder", "LeftHip"));
+        boneEdgeNames.Add("RightFlank", ("RightShoulder", "RightHip"));
+        boneEdgeNames.Add("Pelvis", ("LeftHip", "RightHip"));
+        boneEdgeNames.Add("LeftThigh", ("LeftHip", "LeftKnee"));
+        boneEdgeNames.Add("RightTigh", ("RightHip", "RightKnee"));
+        boneEdgeNames.Add("LeftShin", ("LeftKnee", "LeftAnkle"));
+        boneEdgeNames.Add("RightShin", ("RightKnee", "RightAnkle"));
         
         //Initialize bone objects
-        // foreach(KeyValuePair<string, (string, string)> boneEdgeName in boneEdgeNames)
-        // {
-        //     // InstantiateCylinder(
-        //     //     cylinderPrefab, 
-        //     //     jointGameObjects[boneEdgeName.Value.Item1].transform.position, 
-        //     //     jointGameObjects[boneEdgeName.Value.Item2].transform.position
-        //     //     );
-        //     // LineRendererコンポーネントをゲームオブジェクトにアタッチする
-        //     // Debug.Log(typeof(boneEdgeName.Value.Item1));
-        //     string startJointName = boneEdgeName.Value.Item1;
-        //     string endJointName = boneEdgeName.Value.Item2;
-        //     Debug.Log(startJointName);
-        //     Debug.Log(jointGameObjects.Keys);
-
-        //     var lineRenderer = gameObject.AddComponent<LineRenderer>();
-
-        //     var positions = new Vector3[] {
-        //         jointGameObjects[startJointName].transform.position,               // 開始点
-        //         jointGameObjects[endJointName].transform.position,               // 終了点
-        //     };
-
-        //     // 線を引く場所を指定する
-        //     lineRenderer.SetPositions(positions);
-        // }
-        foreach(KeyValuePair<string, GameObject> jointItem in jointGameObjects) {
-            string jointName = jointItem.Key;
-            Debug.Log(jointName);
+        foreach(KeyValuePair<string, (string, string)> boneEdgeName in boneEdgeNames)
+        {
+            string boneName = boneEdgeName.Key;
+            // Debug.Log(boneKey);
+            string startJointName = boneEdgeName.Value.Item1;
+            string endJointName = boneEdgeName.Value.Item2;
+            InstantiateCylinder(
+                boneName,
+                cylinderPrefab,
+                jointGameObjects[startJointName].transform.position, 
+                jointGameObjects[endJointName].transform.position
+                );
+            boneGameObjects[boneName].GetComponent<Renderer>().material.color = jointColor;
         }
-
-        // foreach (var cylinderPrefab in cylinderPrefabs)
-        // {
-        //     InstantiateCylinder(cylinderPrefab, new Vector3(-1, 0, 0), new Vector3(1, 0, 0));
-        // }
+        Debug.Log("boneName");
+        foreach(var boneName in boneGameObjects.Keys){
+            // Debug.Log(boneName);
+        }
+        Debug.Log("boneName");
     }
 
     // Update is called once per frame
     void Update()
     {
-
         //以下FPS関連
         timeElapsed += Time.deltaTime;
         //FPSを制御
@@ -137,7 +131,7 @@ class nose : MonoBehaviour
                 Transform jointTransform = jointGameObjects[jointName].transform;
                 // Transform myTransform = myTransforms[jointIdx];
 
-                if (frameCount == 0){
+                if (frameCount == 0 && loopCount == 0){
                     // Debug.Log("ここだぜ");
                     lastFramePoses.Add(jointName,jointTransform.position);
                 }
@@ -157,6 +151,19 @@ class nose : MonoBehaviour
                 
             }
             //ここでcylのupdate
+            foreach(KeyValuePair<string, (string, string)> boneEdgeName in boneEdgeNames) {
+                string boneName = boneEdgeName.Key;
+                string startJointName = boneEdgeName.Value.Item1;
+                string endJointName = boneEdgeName.Value.Item2;
+                Debug.Log(boneName);
+                // Debug.Log(jointGameObjects[startJointName].transform.position);
+                // Debug.Log(startJointName);
+                UpdateCylinderPosition(
+                    boneGameObjects[boneName],
+                    jointGameObjects[startJointName].transform.position, 
+                    jointGameObjects[endJointName].transform.position
+                    );
+            }
 
             // カウンタをインクリメント
             frameCount += 1;
@@ -176,22 +183,22 @@ class nose : MonoBehaviour
             timeElapsed = 0.0f;
         }
     }
-    private void InstantiateCylinder(Transform cylinderPrefab, Vector3 beginPoint, Vector3 endPoint)
+    private void InstantiateCylinder(String key, Transform cylinderPrefab, Vector3 beginPoint, Vector3 endPoint)
     {
-        cylinder = Instantiate<GameObject>(cylinderPrefab.gameObject, Vector3.zero, Quaternion.identity);
-        UpdateCylinderPosition(cylinder, beginPoint, endPoint);
+        boneGameObjects[key] = Instantiate<GameObject>(cylinderPrefab.gameObject, Vector3.zero, Quaternion.identity);
+        UpdateCylinderPosition(boneGameObjects[key], beginPoint, endPoint);
     }
 
-    private void UpdateCylinderPosition(GameObject cylinder, Vector3 beginPoint, Vector3 endPoint)
+    private void UpdateCylinderPosition(GameObject cyl, Vector3 beginPoint, Vector3 endPoint)
     {
         Vector3 offset = endPoint - beginPoint;
         Vector3 position = beginPoint + (offset / 2.0f);
 
-        cylinder.transform.position = position;
-        cylinder.transform.LookAt(beginPoint);
-        Vector3 localScale = cylinder.transform.localScale;
+        cyl.transform.position = position;
+        cyl.transform.LookAt(beginPoint);
+        Vector3 localScale = cyl.transform.localScale;
         localScale.z = (endPoint - beginPoint).magnitude;
-        cylinder.transform.localScale = localScale;
+        cyl.transform.localScale = localScale;
     }
 }
 
