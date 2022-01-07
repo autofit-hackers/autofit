799bf6beedb5b30082138ea7e1aa67a74f8f8c25
Yusuke Kondo <kondounagi@gmail.com>
2022-01-07 09:31:23 +0900

    feat: renew rigid alignment algorithm (normal vector ver.)

--- csvAnimationConverter-master/Assets/rigidAlignmentBaseline/rigidAlignmentBaseline.cs
+++ csvAnimationConverter-master/Assets/rigidAlignmentBaseline/rigidAlignmentBaseline.cs
@@ -2,210 +2,91 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine; // for vector3
-using Numpy; // ref. https://github.com/SciSharp/Numpy.NET
 
-namespace RigidAlignment
+namespace RigidAlignmentBaseline
 {
-    public class RigidAlignment
+    public class RigidAlignmentBaseline
     {
-        public List<float[]> rigidTransformFactors;
-        public Vector3 transformedTo;
-        public Vector3 computedRotation;
-        public Vector3 translation;
+        public Dictionary<string, Vector3> fromJoints;
+        public Dictionary<string, Vector3> toJoints;
+        private Vector3 fromNormalVector;
+        private Vector3 toNormalVector;
+        private Vector3 fromCenterOfGravity;
+        private Vector3 toCenterOfGravity;
+        private Quaternion fromToRotation = new Quaternion();
         
-        public RigidAlignment(Dictionary<string, Vector3> fromKeyFrame, Dictionary<string, Vector3> toKeyFrame)
+        public RigidAlignmentBaseline(Dictionary<string, Vector3> fromKeyFrame, Dictionary<string, Vector3> toKeyFrame)
         {
-            rigidTransformFactors = CalcRigidTransformMatrix(fromKeyFrame, toKeyFrame);
-            // var mse = arrayToVec3(rigidTransformFactors[0]);
-            transformedTo = ArrayToVec3(rigidTransformFactors[1]);
-            Debug.Log("transformedTo");
-            Debug.Log(transformedTo);
-            computedRotation = ArrayToVec3(rigidTransformFactors[2]);
-            Debug.Log("computedRotation");
-            Debug.Log(computedRotation);
-            // var scaling = arrayToVec3(rigidTransformFactors[3]);
-            translation = ArrayToVec3(rigidTransformFactors[4]);
-            Debug.Log("translation");
-            Debug.Log(translation);
-            // Debug.Log("scaling");
-            // Debug.Log(scaling);
+            fromJoints = fromKeyFrame;
+            toJoints = toKeyFrame;
+            fromNormalVector = CalcNormalVector(fromJoints);
+            toNormalVector = CalcNormalVector(toJoints);
+            fromCenterOfGravity = CalcCenterOfGravityVector(fromJoints);
+            toCenterOfGravity = CalcCenterOfGravityVector(toJoints);
+            fromToRotation.SetFromToRotation(fromNormalVector, toNormalVector);
+        }
+
+        public Vector3 CalcNormalVector(Dictionary<string, Vector3> joints)
+        {
+            // 肩の2点と腰の1点（腰は2点の平均座標）が張る平面に直行する法線ベクトルを得る
+            // ref. https://docs.unity3d.com/ja/2019.4/Manual/ComputingNormalPerpendicularVector.html
+            var wristVector = Vector3.Lerp(joints["LeftWrist"], joints["RightWrist"], 0.5f);
+            var side1 = joints["RightShoulder"] - wristVector;
+            var side2 = joints["LeftShoulder"] - wristVector;
+            var perp = Vector3.Cross(side1, side2);
+            return perp.normalized;
         }
         
-        public  List<Dictionary<string, Vector3>> RigidTransformFrames(List<Dictionary<string, Vector3>> fromFrames)
+        public Vector3 CalcCenterOfGravityVector(Dictionary<string, Vector3> joints)
+        {
+            var centerOfGravity = Vector3.zero;
+            foreach (var joint in joints.Values)
+            {
+                centerOfGravity += joint;
+            }
+            return centerOfGravity / joints.Count;
+        }
+
+        static Vector3 RotateAroundPoint(Vector3 point, Vector3 pivot, Quaternion rotation)
+        {
+            var finalPos = point - pivot;
+            // Center the point around the origin
+            finalPos = rotation * finalPos;
+            //Rotate the point.
+            finalPos += pivot;
+            return finalPos;
+        }
+
+        public Dictionary<string, Vector3> RigidTransformJoints(Dictionary<string, Vector3> joints, Vector3 pivot, Quaternion rotation)
+        {
+            Dictionary<string, Vector3> rotatedJoints = new Dictionary<string, Vector3>();
+            foreach (KeyValuePair<string, Vector3> joint in joints)
+            {
+                rotatedJoints.Add(joint.Key, RotateAroundPoint(joint.Value, pivot, rotation));
+            }
+            return rotatedJoints;
+        }
+        
+        public List<Dictionary<string, Vector3>> RigidTransformFrames(List<Dictionary<string, Vector3>> fromFrames)
         {
             List<Dictionary<string, Vector3>> transformedFrames = new List<Dictionary<string, Vector3>>();
 
             for (int i = 0; i < fromFrames.Count; i++)
             {
-                var fromJointPositions = fromFrames[i];
+                var _fromJoints = fromFrames[i];
                 Dictionary<string, Vector3> transformedJoints = new Dictionary<string, Vector3>();
 
-                foreach (var jointName in jointNames)
+                foreach (KeyValuePair<string, Vector3> fromJoint in _fromJoints)
                 {
-                    var fromJointPos = fromJointPositions[jointName];
-                    var transformedJoint = fromJointPos + translation;
-                    transformedJoints.Add(jointName, transformedJoint);
+                    // 重心位置をtoに合わせる
+                    var transformedJoint = fromJoint.Value - this.fromCenterOfGravity + this.toCenterOfGravity ;
+                    // toまわりに回転させる
+                    transformedJoints.Add(fromJoint.Key, RotateAroundPoint(transformedJoint, this.toCenterOfGravity, this.fromToRotation));
                 }
-                
-                // Dictionary<string, Vector3> transformedOneFrame = new Dictionary<string, Vector3>();
-                // foreach (KeyValuePair<string, Vector3> fromJointItem in fromJointPositions)
-                // {
-                //     var fromJointName = fromJointItem.Key;
-                //     var fromJointPos = fromJointItem.Value;
-                //     var transformedJointPos = fromOneFrame;
-                //     transformedOneFrame.Add(fromJointName, transformedJointPos);
-                // }
-
                 transformedFrames.Add(transformedJoints);
             }
             return transformedFrames;
         }
-
-        private static Vector3 ArrayToVec3(float[] xyzArr)
-        {
-            return new Vector3(x: xyzArr[0], y: xyzArr[1], xyzArr[2]);
-        }
-
-        private static float[,] JointsToArray(Dictionary<string, Vector3> joints)
-        {
-            Debug.Log(string.Join(",", joints.Keys));
-            float[,] jointsArr = new float[numOfJoints,3];
-            foreach (var it in jointNames.Select((x, i) => new {Value = x, Index = i}))
-            {
-                var idx = it.Index;
-                var jointName = it.Value;
-                var joint = joints[jointName];
-                // jointsArr[idx] = new float[] {joint.x, joint.y, joint.z};
-                jointsArr[idx, 0] = joint.x;
-                jointsArr[idx, 1] = joint.y;
-                jointsArr[idx, 2] = joint.z;
-            }
-            return jointsArr;
-        }
-        
-        private static Dictionary<string, Vector3> ArrayToJoints(float[][] jointsArr)
-        {
-            Dictionary<string, Vector3> joints = new Dictionary<string, Vector3>();
-            foreach (var it in jointNames.Select((x, i) => new {Value = x, Index = i}))
-            {
-                var idx = it.Index;
-                var jointName = it.Value;
-                Vector3 jointVec = new Vector3(x: jointsArr[idx][0], y: jointsArr[idx][1], z: jointsArr[idx][2]);
-                joints.Add(jointName, jointVec);
-            }
-            return joints;
-        }
-        
-        private static List<float[]> CalcRigidTransformMatrix(Dictionary<string, Vector3> fromJoints, Dictionary<string, Vector3> toJoints)
-        {
-            Debug.Log("CalcRigidTransformMatrix");
-            float[,] fromJointsArr = JointsToArray(fromJoints);
-            float[,] toJointsArr = JointsToArray(toJoints);
-            Debug.Log("fromJointsArr");
-            Debug.Log(fromJointsArr);
-            Debug.Log("toJointsArr");
-            Debug.Log(toJointsArr);
-
-            var fromJointsNp = np.array(fromJointsArr);
-            var toJointsNp = np.array(toJointsArr);
-            
-            /*
-            ref. https://github.com/una-dinosauria/3d-pose-baseline/blob/666080d86a96666d499300719053cc8af7ef51c8/src/procrustes.py
-            A port of MATLAB's `procrustes` function to Numpy.
-            Adapted from http://stackoverflow.com/a/18927641/1884420
-            Args
-                from: array NxM of targets, with N number of points and M point dimensionality
-                to: array NxM of inputs
-                compute_optimal_scale: whether we compute optimal scale or force it to be 1
-            Returns:
-                d: squared error after transformation
-                Z: transformed to
-                T: computed rotation
-                b: scaling
-                c: translation
-            */
-
-            var muFrom = fromJointsNp.mean(0);
-            var muTo = toJointsNp.mean(0);
-
-            var from0 = fromJointsNp - muFrom;
-            var to0 = toJointsNp - muTo;
-
-            var ssFrom = (from0 * from0).sum();
-            var ssTo = (to0 * to0).sum();
-
-            // centred Frobenius norm
-            var normfrom = np.sqrt(ssFrom);
-            var normto = np.sqrt(ssTo);
-
-            // scale to equal (unit) norm
-            from0 = from0 / normfrom;
-            to0 = to0 / normto;
-
-            // optimum rotation matrix of to
-            var A = np.dot(from0.T, to0);
-            (NDarray, NDarray, NDarray) svdResult = np.linalg.svd(A, full_matrices: true); //  full_matrices = False);
-            var U = svdResult.Item1;
-            var s = svdResult.Item2;
-            var Vt = svdResult.Item3;
-            
-            var V = Vt.T;
-            var T = np.dot(V, U.T);
-
-            // Make sure we have a rotation
-            var detT = np.linalg.det(T);
-            V[":, -1"].imul(np.sign(detT));
-            s["-1"].imul(np.sign(detT));
-            T = np.dot(V, U.T);
-
-            var traceTA = s.sum();
-
-            // Compute optimum scaling of to.
-            var b = traceTA * normfrom / normto;
-            var d = 1.0 - traceTA * traceTA;
-            var Z = normfrom * traceTA * np.dot(to0, T) + muFrom;
-
-            var c = muFrom - b * np.dot(muTo, T);
-
-            return new List<float[]>{d.GetData<float>(), Z.GetData<float>(), T.GetData<float>(), b.GetData<float>(), c.GetData<float>()};
-        }
-
-        static List<string> jointNames = new List<string> {
-            "Nose",
-            "LeftEyeInner",
-            "LeftEye",
-            "LeftEyeOuter",
-            "RightEyeInner",
-            "RightEye",
-            "RightEyeOuter",
-            "LeftEar",
-            "RightEar",
-            "MouthLeft",
-            "MouthRight",
-            "LeftShoulder",
-            "RightShoulder",
-            "LeftElbow",
-            "RightElbow",
-            "LeftWrist",
-            "RightWrist",
-            "LeftPinkyFinger",
-            "RightPinkyFinger",
-            "LeftIndexFinger",
-            "RightIndexFinger",
-            "LeftThumb",
-            "RightThumb",
-            "LeftHip",
-            "RightHip",
-            "LeftKnee",
-            "RightKnee",
-            "LeftAnkle",
-            "RightAnkle",
-            "LeftHeel",
-            "RightHeel",
-            "LeftToe",
-            "RightToe"
-        };
-        private static int numOfJoints = jointNames.Count;
     }
 }
 
