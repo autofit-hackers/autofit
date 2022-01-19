de8c47897adada846046a3c5e84cfb8ce3a09672
Yusuke Kondo <kondounagi@gmail.com>
2022-01-20 02:12:55 +0900

    chore: configure startFrame and end frame from unity console

--- posefit-visualizer/Assets/Scripts/Main.cs
+++ posefit-visualizer/Assets/Scripts/Main.cs
@@ -13,6 +13,10 @@ class Main : MonoBehaviour
     public string initJsonFilePath = "./Assets/Data/sq-1228_2.json"; // "./Assets/Data/init-20220119.json";
     public string jsonFilePath1 = "./Assets/Data/sq-1231.json";
     public string jsonFilePath2 = "./Assets/Data/sq-1228.json";
+    public int startFrame1 = 750;
+    public int endFrame1 = 1100;
+    public int startFrame2 = 80;
+    public int endFrame2 = 220;
     private Dictionary<string, BoneOrdinal> boneOrdinals;
     private HumanMotion hm1;
     private HumanMotion hm2;
@@ -27,8 +31,8 @@ class Main : MonoBehaviour
             Debug.Log(boneOrdinal.Value.boneLength);
         }
 
-        hm1 = new HumanMotion(jsonFilePath: jsonFilePath1, boneOrdinals: boneOrdinals, cylinderPrefab: cylinderPrefab, spherePrefab: spherePrefab, jointColor: Color.cyan, startFrame: 750, endFrame: 1100);
-        hm2 = new HumanMotion(jsonFilePath: jsonFilePath2, boneOrdinals: boneOrdinals, cylinderPrefab: cylinderPrefab, spherePrefab: spherePrefab, jointColor: Color.magenta, startFrame: 80, endFrame: 220);
+        hm1 = new HumanMotion(jsonFilePath: jsonFilePath1, boneOrdinals: boneOrdinals, cylinderPrefab: cylinderPrefab, spherePrefab: spherePrefab, jointColor: Color.cyan, startFrame: startFrame1, endFrame: endFrame1);
+        hm2 = new HumanMotion(jsonFilePath: jsonFilePath2, boneOrdinals: boneOrdinals, cylinderPrefab: cylinderPrefab, spherePrefab: spherePrefab, jointColor: Color.magenta, startFrame: startFrame2, endFrame: endFrame2);
         hm1.ReadAndPreprocess();
         hm2.ReadAndPreprocess();
     }
