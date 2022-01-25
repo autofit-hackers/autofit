183665da61ed47d6297adbbfd636037d8c554794
Yusuke Kondo <kondounagi@gmail.com>
2022-01-25 20:35:33 +0900

    chore: add BlazePoseBarracuda

---
+++ BlazePoseBarracuda/Assets/Script/WebCamInput.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+
+public class WebCamInput : MonoBehaviour
+{
+    [SerializeField] string webCamName;
+    [SerializeField] Vector2 webCamResolution = new Vector2(1920, 1080);
+    [SerializeField] Texture staticInput;
+    public int deviceId = 0;
+    public int fps = 30;
+
+    // Provide input image Texture.
+    public Texture inputImageTexture{
+        get{
+            if(staticInput != null) return staticInput;
+            return inputRT;
+        }
+    }
+
+    WebCamTexture webCamTexture;
+    RenderTexture inputRT;
+
+    void Start()
+    {
+        if(staticInput == null){
+            WebCamDevice[] devices = WebCamTexture.devices;
+            webCamTexture = new WebCamTexture(devices[deviceId].name, (int)webCamResolution.x, (int)webCamResolution.y, fps);
+            webCamTexture.Play();
+        }
+
+        inputRT = new RenderTexture((int)webCamResolution.x, (int)webCamResolution.y, 0);
+    }
+
+    void Update()
+    {
+        if(staticInput != null) return;
+        if(!webCamTexture.didUpdateThisFrame) return;
+
+        var aspect1 = (float)webCamTexture.width / webCamTexture.height;
+        var aspect2 = (float)inputRT.width / inputRT.height;
+        var aspectGap = aspect2 / aspect1;
+
+        var vMirrored = webCamTexture.videoVerticallyMirrored;
+        var scale = new Vector2(aspectGap, vMirrored ? -1 : 1);
+        var offset = new Vector2((1 - aspectGap) / 2, vMirrored ? 1 : 0);
+
+        Graphics.Blit(webCamTexture, inputRT, scale, offset);
+    }
+
+    void OnDestroy(){
+        if (webCamTexture != null) Destroy(webCamTexture);
+        if (inputRT != null) Destroy(inputRT);
+    }
+}
---
+++ BlazePoseBarracuda/Packages/BlazePoseBarracuda/CHANGELOG.md
@@ -0,0 +1,9 @@
+### [1.1.1] - 2021-10-04
+Change the install method from GitHub URL to [npmjs.com](https://www.npmjs.com/).
+Source code change is nothing.
+
+### [1.1.0] - 2021-07-21
+Support 3D world landmarks of MediaPipe Pose v0.8.6
+
+### [1.0.0] - 2021-05-30
+This is the first release of `creativeIKEP/BlazePoseBarracuda`(`jp.ikep.mediapipe.blazepose`).
