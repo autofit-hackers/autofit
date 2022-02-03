509fc6b1eca7aebda420a9b196bfdc7904818c52
s-endo17 <yattsa0902@gmail.com>
2022-02-03 22:13:35 +0900

    feat: count reps

--- posefit-visualizer/Assets/Scripts/Reference/PoseVisuallizer.cs
+++ posefit-visualizer/Assets/Scripts/Reference/PoseVisuallizer.cs
@@ -15,6 +15,8 @@ public class PoseVisuallizer : MonoBehaviour
 
     Material material;
     BlazePoseDetecter detecter;
+    private int flag = 0;
+    private int repCount = 0;
 
     // Lines count of body's topology.
     const int BODY_LINE_NUM = 35;
@@ -36,15 +38,97 @@ public class PoseVisuallizer : MonoBehaviour
         detecter = new BlazePoseDetecter(blazePoseResource, poseLandmarkModel);
     }
 
+//      void LateUpdate()
+//      {
+//          Texture input = webCamInput.inputImageTexture; // Your input image texture
+//
+//        // Predict pose by neural network model.
+//        // Switchable anytime between neural network models with 2nd argment.
+//        detecter.ProcessImage(input, poseLandmarkModel);
+//
+//        /*
+//        `detecter.outputBuffer` is pose landmark result and ComputeBuffer of float4 array type.
+//        0~32 index datas are pose landmark.
+//            Check below Mediapipe document about relation between index and landmark position.
+//            https://google.github.io/mediapipe/solutions/pose#pose-landmark-model-blazepose-ghum-3d
+//            Each data factors are
+//            x: x cordinate value of pose landmark ([0, 1]).
+//            y: y cordinate value of pose landmark ([0, 1]).
+//            z: Landmark depth with the depth at the midpoint of hips being the origin.
+//               The smaller the value the closer the landmark is to the camera. ([0, 1]).
+//               **The use of this value is not recommended. You can use `worldLandmarkBuffer` if z value is needed.**
+//            w: The score of whether the landmark position is visible ([0, 1]).
+//
+//        33 index data is the score whether human pose is visible ([0, 1]).
+//        This data is (score, 0, 0, 0).
+//        */
+//        ComputeBuffer result = detecter.outputBuffer;
+//
+//        /*
+//        `detecter.worldLandmarkBuffer` is pose world landmark result and ComputeBuffer of float4 array type.
+//        0~32 index datas are pose world landmark.
+//            Check below Mediapipe document about relation between index and landmark position.
+//            https://google.github.io/mediapipe/solutions/pose#pose-landmark-model-blazepose-ghum-3d
+//            Each data factors are
+//            x, y and z: Real-world 3D coordinates in meters with the origin at the center between hips.
+//            w: The score of whether the world landmark position is visible ([0, 1]).
+//
+//        33 index data is the score whether human pose is visible ([0, 1]).
+//        This data is (score, 0, 0, 0).
+//        */
+//        ComputeBuffer worldLandmarkResult = detecter.worldLandmarkBuffer;
+//
+//        // `detecter.vertexCount` is count of pose landmark vertices.
+//        // `detecter.vertexCount` returns 33.
+//        int count = detecter.vertexCount;
+//
+//        // Your custom processing from here, i.e. rendering.
+//        // For example, below is CPU log debug.
+//        var data = new Vector4[count];
+//        result.GetData(data);
+//        Debug.Log("---");
+//        foreach(var d in data){
+//          Debug.Log(d);
+//        }
+//
+//        worldLandmarkResult.GetData(data);
+//        Debug.Log("---");
+//        foreach(var d in data){
+//          Debug.Log(d);
+//        }
+//      }
+
     void LateUpdate(){
         inputImageUI.texture = webCamInput.inputImageTexture;
 
         // Predict pose by neural network model.
         // Switchable anytime models with 2nd argment.
         detecter.ProcessImage(webCamInput.inputImageTexture, poseLandmarkModel);
+        ComputeBuffer result = detecter.outputBuffer;
+        ComputeBuffer worldLandmarkResult = detecter.worldLandmarkBuffer;
         
-        Debug.Log(detecter.outputBuffer);
+        int count = detecter.vertexCount;
+        var data = new Vector4[count];
+        result.GetData(data);
+        if (flag == 0 && data[16].y < 0.4) flag = 1;
+        if (flag == 1 && data[16].y > 0.7)
+        {
+            flag = 0;
+            repCount += 1;
+        }
+        Debug.Log(repCount);
+        
+        // foreach(var d in data){
+        //     Debug.Log(d);
+        // }
+
+        // worldLandmarkResult.GetData(data);
+        // Debug.Log("---");
+        // foreach(var d in data){
+        //     Debug.Log(d);
+        // }
     } 
+
 
     void OnRenderObject(){
         var w = inputImageUI.rectTransform.rect.width;
