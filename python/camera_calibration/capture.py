7d978bedc3e3c25d2da744df4a04be2157cafbb6
Yusuke Kondo <kondounagi@gmail.com>
2022-04-07 21:50:38 +0900

    refactor: use argparse

--- python/camera_calibration/capture.py
+++ python/camera_calibration/capture.py
@@ -1,45 +1,53 @@
-import sys
+import argparse
 import time
 
 import cv2
 import numpy as np
 
-# put webcam id as command line arguements
-captures = []
-if len(sys.argv) >= 2:
-    for idx, arg in enumerate(sys.argv[1:]):
-        captures.append(cv2.VideoCapture(int(arg)))
-        print(f"camera_{idx} is available")
-else:
-    print("Call program with input webcam!")
-    quit()
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--camera-id", type=int, nargs="+", help="Camera device id")
+    return parser.parse_args()
 
-# wait cameras to wake up
-for i in range(3):
-    print(f"start in {3-i} sec")
-    t0 = time.time()
-    time.sleep(1)
-    t1 = time.time()
 
-n = 0
-print("press c to take photo or esc to exit")
-while True:
-    frames = []
+def main():
+    args = get_args()
+    # put webcam id as command line arguements
+    captures = []
+    for idx, camera_id in enumerate(args.camera_id):
+        captures.append(cv2.VideoCapture(int(camera_id)))
+        print(f"Camera No.{idx} is available: device id = {camera_id}")
+
+    # wait cameras to wake up
+    for i in range(3):
+        print(f"start in {3-i} sec")
+        t0 = time.time()
+        time.sleep(1)
+        t1 = time.time()
+
+    n = 0
+    print("press c to take photo or esc to exit")
+    while True:
+        frames = []
+        for capture in captures:
+            ret, frame = capture.read()
+            frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
+            frames.append(frame)
+        mergeImg = np.vstack(frames)
+        cv2.imshow("webcam inputs", mergeImg)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("c"):
+            for idx, frame in enumerate(frames):
+                cv2.imwrite(f"camera{idx}_img{n}.png", frame)
+            n += 1
+            print(f"images_{n} has been captured")
+        elif key == 27:
+            break
+
     for capture in captures:
-        ret, frame = capture.read()
-        frame = cv2.rotate(frame, cv2.ROTATE_90_COUNTERCLOCKWISE)
-        frames.append(frame)
-    mergeImg = np.vstack(frames)
-    cv2.imshow("webcam inputs", mergeImg)
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord("c"):
-        for idx, frame in enumerate(frames):
-            cv2.imwrite(f"camera{idx}_img{n}.png", frame)
-        n += 1
-        print(f"images_{n} has been captured")
-    elif key == 27:
-        break
+        capture.release()
+    cv2.destroyAllWindows()
 
-for capture in captures:
-    capture.release()
-cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
