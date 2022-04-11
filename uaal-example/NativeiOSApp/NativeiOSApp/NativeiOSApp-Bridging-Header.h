2fbe337d714162d4a4faf041170e4530819c23a0
Yusuke Kondo <kondounagi@gmail.com>
2022-04-11 11:23:37 +0900

    chore: remove unused directories

--- uaal-example/NativeiOSApp/NativeiOSApp/NativeiOSApp-Bridging-Header.h
+++
@@ -1,9 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-//#include "UnityFramework/UnityFramework.h"
-//#include <UnityFramework/NativeCallProxy.h>
-
-@class @"/Frameworks/UnityFramework.framework";
-#import "UnityFramework/NativeCallProxy.h";
