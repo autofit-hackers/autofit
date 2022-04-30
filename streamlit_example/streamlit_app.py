3268710540adb63c5fb636a3c49036bd51604337
masaueno <ueno-masaki202@g.ecc.u-tokyo.ac.jp>
2022-04-30 20:07:48 +0900

    feat: 3d pose reconstruction

--- streamlit_example/streamlit_app.py
+++ streamlit_example/streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-from apps import (calibration_app,  # import your app modules here
-                  get_physical_info_app, make_session_app, pose_app)
+from apps import calibration_app  # import your app modules here
+from apps import get_physical_info_app, make_session_app, pose3d_reconstruction, pose_app
 from utils import MultiApp
 
 
@@ -13,6 +13,7 @@ def main():
     app.add_app("Calibration", calibration_app.app)
     app.add_app("Pose", pose_app.app)
     app.add_app("Get physical info", get_physical_info_app.app)
+    app.add_app("Reconstruct and vizualize 3D Pose", pose3d_reconstruction.app)
     # TODO: pkl編集画面を立てる
 
     # The main app
--- streamlit_example/utils/calib_cam.py
+++ streamlit_example/utils/calib_cam.py
@@ -1,8 +1,8 @@
 import argparse
 import glob
 import os
-from pathlib import Path
 from dataclasses import dataclass
+from pathlib import Path
 from typing import Any, List, NamedTuple, Tuple, Union
 
 import cv2 as cv
@@ -224,30 +224,10 @@ def DLT(P1, P2, point1, point2):
         P2[0, :] - point2[0] * P2[2, :],
     ]
     A = np.array(A).reshape((4, 4))
-    # print('A: ')
-    # print(A)
-
     B = A.transpose() @ A
-
     U, s, Vh = linalg.svd(B, full_matrices=False)
 
-    # print('Triangulated point: ')
-    # print(Vh[3,0:3]/Vh[3,3])
     return Vh[3, 0:3] / Vh[3, 3]
-
-
-def read_camera_parameters(camera_id, savefolder="camera_parameters/"):
-    cmtx = np.loadtxt(savefolder + "c" + str(camera_id) + "_mtx.dat")
-    dist = np.loadtxt(savefolder + "c" + str(camera_id) + "_dist.dat")
-
-    return cmtx, dist
-
-
-def read_rotation_translation(camera_id, savefolder="camera_parameters/"):
-    rot = np.loadtxt(savefolder + "c" + str(camera_id) + "_rot.dat")
-    trans = np.loadtxt(savefolder + "c" + str(camera_id) + "_trans.dat")
-
-    return rot, trans
 
 
 # def _convert_to_homogeneous(pts):
@@ -259,13 +239,14 @@ def read_rotation_translation(camera_id, savefolder="camera_parameters/"):
 #         return np.concatenate([pts, [1]], axis=0)
 
 
-def get_projection_matrix(camera_id):
+def get_projection_matrix(camera_info_path: Path, camera_type: str):
     # read camera parameters
-    cmtx, dist = read_camera_parameters(camera_id)
-    rvec, tvec = read_rotation_translation(camera_id)
-
+    cmtx = np.loadtxt(Path(f"{camera_info_path}/{camera_type}/mtx.dat"))
+    dist = np.loadtxt(Path(f"{camera_info_path}/{camera_type}/dist.dat"))
+    rot = np.loadtxt(Path(f"{camera_info_path}/{camera_type}/rot.dat"))
+    trans = np.loadtxt(Path(f"{camera_info_path}/{camera_type}/trans.dat"))
     # calculate projection matrix
-    P = cmtx @ _make_homogeneous_rep_matrix(rvec, tvec)[:3, :]
+    P = cmtx @ _make_homogeneous_rep_matrix(rot, trans)[:3, :]
     return P
 
 
