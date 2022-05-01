fix: apply cam rotation in video saving

streamlit_example/processor/pose_processor.py
@@ -258,22 +258,22 @@ class PoseProcessor(VideoProcessorBase):
         self._in_queue.put_nowait(_SENTINEL_)
         self._pose_process.join(timeout=10)
 
-    def _create_video_writer(self, save_path: str, fps: int, frame: av.VideoFrame) -> cv.VideoWriter:
+    def _create_video_writer(self, save_path: str, fps: int, frame: np.ndarray) -> cv.VideoWriter:
         """Save video as mp4."""
         os.makedirs(os.path.dirname(save_path), exist_ok=True)
         fourcc = cv.VideoWriter_fourcc("m", "p", "4", "v")
-        video = cv.VideoWriter(save_path, fourcc, fps, (frame.width, frame.height))
+        print(frame.shape)
+        video = cv.VideoWriter(save_path, fourcc, fps, (tuple(frame.shape[:2])))
         return video
+
+    def _release_video_writer(self) -> None:
+        print("Releasing video_writer...")
+        self.video_writer.release()
+        self.video_writer = None
+        print(f"Video has saved to {self.video_save_path}")
 
     def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
         display_fps = self._FpsCalculator.get()
-
-        # 動画の保存（初期化）
-        if (self.save_state.is_saving_video) and (self.video_writer is None) and (self.video_save_path is not None):
-            # video_writer の初期化
-            # TODO: fps は 30 で決め打ちしているが、実際には処理環境に応じて変化する
-            assert self.video_save_path is not None
-            self.video_writer = self._create_video_writer(save_path=self.video_save_path, fps=30, frame=frame)
 
         # カメラキャプチャ #####################################################
         frame = frame.to_ndarray(format="bgr24")
@@ -291,16 +291,22 @@ class PoseProcessor(VideoProcessorBase):
             cv.imwrite(self.skeleton_save_path, frame)
             self.capture_skeleton = False
 
-        # 動画の保存（フレームの追加）
-        if self.save_state.is_saving_video and self.video_writer:
+        # 動画の保存（初期化
+        # if (self.save_state.is_saving_video) and (self.video_writer is None) and (self.video_save_path is not None):
+        if self.save_state.is_saving_video:
+            if self.video_writer is None:
+                # video_writer の初期化
+                # TODO: fps は 30 で決め打ちしているが、実際には処理環境に応じて変化する
+                assert self.video_save_path is not None
+                self.video_writer = self._create_video_writer(save_path=self.video_save_path, fps=30, frame=frame)
+            # 動画の保存（フレームの追加）
             # NOTE: video_writer は cv2 の実装を用いているため、BGRの色順で良い
             self.video_writer.write(frame)
+            print("add frame")
 
         # 動画の保存（writerの解放）
         if (not self.save_state.is_saving_video) and (self.video_writer is not None):
-            print("Releasing video_writer...")
-            self.video_writer.release()
-            self.video_writer = None
+            self._release_video_writer()
 
         # 検出実施 #############################################################
         frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
@@ -415,5 +421,4 @@ class PoseProcessor(VideoProcessorBase):
         if len(self.pose_mem) > 0:
             self._save_pose()
         if self.video_writer is not None:
-            print("Stop writing video process...")
             self.video_writer.release()