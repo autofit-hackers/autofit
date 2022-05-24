a43be7361115d64c13a168a75351a2a2277df770
masaueno <ueno-masaki202@g.ecc.u-tokyo.ac.jp>
2022-05-24 14:05:11 +0900

    refactor: add _ to var names

--- front/utils/voice_recognition.py
+++ front/utils/voice_recognition.py
@@ -17,17 +17,17 @@ class VoiceRecognitionProcess(Process):
         """
         assert stt_api == "vosk" or "sr", f"stt_api must be Vosk or speech_recognition"
         super(VoiceRecognitionProcess, self).__init__()
-        self.stt_api = stt_api
-        self.recognized_voice_queue = Queue()
+        self._stt_api = stt_api
+        self._recognized_voice_queue = Queue()
 
     def run(self):
         """
         automatically executed when the process starts
         """
-        if self.stt_api == "vosk":
+        if self._stt_api == "vosk":
             self._run_recognizer_with_vosk()
 
-        elif self.stt_api == "sr":
+        elif self._stt_api == "sr":
             self._run_recognizer_with_sr()
 
     def _run_recognizer_with_vosk(self):
@@ -36,22 +36,22 @@ class VoiceRecognitionProcess(Process):
         device_info = sd.query_devices(device_id, "input")
         # soundfile expects an int, sounddevice provides a float:
         samplerate = int(device_info["default_samplerate"])
-        _microphone_queue = queue.Queue()
+        microphone_queue = queue.Queue()
 
         def callback(indata, frames, time, status):
             """This is called (from a separate thread) for each audio block."""
             if status:
                 print(status, file=sys.stderr)
-            _microphone_queue.put(bytes(indata))
+            microphone_queue.put(bytes(indata))
 
         with sd.RawInputStream(
             samplerate=samplerate, blocksize=8000, device=device_id, dtype="int16", channels=1, callback=callback
         ):
             rec = vosk.KaldiRecognizer(model, samplerate)
             while True:
-                data = _microphone_queue.get()
+                data = microphone_queue.get()
                 if rec.AcceptWaveform(data):
-                    self.recognized_voice_queue.put_nowait(rec.Result())
+                    self._recognized_voice_queue.put_nowait(rec.Result())
                 else:
                     pass
 
@@ -63,7 +63,7 @@ class VoiceRecognitionProcess(Process):
                 audio = r.listen(source)
             try:
                 text = r.recognize_google(audio, language="ja-JP")
-                self.recognized_voice_queue.put_nowait(text)
+                self._recognized_voice_queue.put_nowait(text)
             except:
                 pass
 
@@ -78,4 +78,4 @@ class VoiceRecognitionProcess(Process):
         return False
 
     def _get_recognized_voice(self):
-        return self.recognized_voice_queue.get_nowait()
+        return self._recognized_voice_queue.get_nowait()
