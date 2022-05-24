a1da1ccc33279ba64d1423bf5be40f38e5a8a298
masaueno <ueno-masaki202@g.ecc.u-tokyo.ac.jp>
2022-05-24 11:57:33 +0900

    refactor: change innner queue var name of stt

--- front/utils/voice_recognition.py
+++ front/utils/voice_recognition.py
@@ -27,20 +27,20 @@ class VoiceRecognitionProcess(Process):
             device_info = sd.query_devices(device_id, "input")
             # soundfile expects an int, sounddevice provides a float:
             samplerate = int(device_info["default_samplerate"])
-            microphone_queue = queue.Queue()
+            _microphone_queue = queue.Queue()
 
             def callback(indata, frames, time, status):
                 """This is called (from a separate thread) for each audio block."""
                 if status:
                     print(status, file=sys.stderr)
-                microphone_queue.put(bytes(indata))
+                _microphone_queue.put(bytes(indata))
 
             with sd.RawInputStream(
                 samplerate=samplerate, blocksize=8000, device=device_id, dtype="int16", channels=1, callback=callback
             ):
                 rec = vosk.KaldiRecognizer(model, samplerate)
                 while True:
-                    data = microphone_queue.get()
+                    data = _microphone_queue.get()
                     if rec.AcceptWaveform(data):
                         out_queue.put_nowait(rec.Result())
                     else:
