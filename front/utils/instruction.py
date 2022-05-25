666365ff690c21d2467b832a86b175ba0dbc5b0d
masaueno <ueno-masaki202@g.ecc.u-tokyo.ac.jp>
2022-05-25 21:08:15 +0900

    refactor: change Instruction class

--- front/utils/instruction.py
+++ front/utils/instruction.py
@@ -4,10 +4,10 @@ from typing import Any, Callable, Dict, List, Tuple
 
 from numpy import ndarray
 
-from utils.class_objects import PoseLandmarksObject, RepObject
+from utils.class_objects import PoseLandmarksObject, RepInfo
 
 
-def squat_knees_in(self, rep_obj: RepObject) -> bool:
+def squat_knees_in(self, rep_obj: RepInfo) -> bool:
     if "bottom" in rep_obj.keyframes.keys():
         bottom_frame_num = rep_obj.keyframes["bottom"]
         bottom_pose = rep_obj.poses[bottom_frame_num]
@@ -17,7 +17,7 @@ def squat_knees_in(self, rep_obj: RepObject) -> bool:
     return False
 
 
-def squat_depth(rep_obj: RepObject) -> bool:
+def squat_depth(rep_obj: RepInfo) -> bool:
     if "bottom" in rep_obj.keyframes.keys():
         bottom_frame_num = rep_obj.keyframes["bottom"]
         bottom_pose = rep_obj.poses[bottom_frame_num]
@@ -28,7 +28,7 @@ def squat_depth(rep_obj: RepObject) -> bool:
 
 
 @dataclass
-class InstructionItem(frozen=True):
+class InstructionInfo(frozen=True):
     text: str
     judge_function: Callable
     reason: str
@@ -36,50 +36,54 @@ class InstructionItem(frozen=True):
 
 
 @dataclass
+class InstructionItem:
+    info: InstructionInfo
+    is_cleared_in_each_rep: List[bool]
+    set_score: float
+
+
+@dataclass
 class Instruction:
-    data: Dict[str, Dict[str, Any]] = {
-        "squat_knees_ahead": {
-            "instruction": InstructionItem(
+    """
+    Add instance variable if you want to define a new instruction.
+    """
+
+    data: Dict[str, InstructionItem] = {
+        "squat_knees_ahead": InstructionItem(
+            info=InstructionInfo(
                 text="内股やな", judge_function=squat_knees_in, reason="外転筋が弱いんちゃうか", menu_to_recommend=["ヒップアブダクション"]
             ),
-            "is_ok": bool,
-            "score": float,
-        },
-        "squat_depth": {
-            "instruction": InstructionItem(
-                text="しゃがめてへんで",
-                judge_function=squat_depth,
-                reason="足首固いんちゃうか",
-                menu_to_recommend=["足首ストレッチ"],
+            is_cleared_in_each_rep=[],
+            set_score=0,
+        ),
+        "squat_depth": InstructionItem(
+            info=InstructionInfo(
+                text="しゃがめてへんで", judge_function=squat_depth, reason="足首固いんちゃうか", menu_to_recommend=["足首ストレッチ"]
             ),
-            "is_ok": bool,
-            "score": float,
-        },
+            is_cleared_in_each_rep=[],
+            set_score=0,
+        ),
     }
 
-
-    def evaluate_rep(self, rep_obj: RepObject):
+    def evaluate_rep(self, rep_obj: RepInfo):
         """rep_objectを全てのinstruction.judge_functionにかけてis_okにboolを代入
 
         Args:
             rep_obj (RepObject): _description_
         """
-        for name, value in self.data.items():
-            value.is_ok = value.(rep_obj)
+        for name, instruction_item in self.data.items():
+            instruction_item.is_cleared_in_each_rep.append(instruction_item.info.judge_function(rep_obj))
 
-    def show_instruction(self, frame: ndarray) -> None:
+    def show(self, frame: ndarray) -> None:
         """frameに指導画像を描画する関数
 
         Args:
             frame (ndarray): フレームだよ
         """
-        # TODO: 上野！たすけて！！
-        for instruction in self.instructions:
-            if instruction.is_ok == False:
-                # 描画する
-                print(instruction.instruction_text)
+        for name, instruction_item in self.data.items():
+            pass
 
 
 #######################################################################
 
-結果 = {"menu": "squat", "weight": 80, "reps": 8, "instructions": 指導}
+# 結果 = {"menu": "squat", "weight": 80, "reps": 8, "instructions": 指導}
