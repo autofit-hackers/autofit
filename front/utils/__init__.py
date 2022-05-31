6721cd2af69ba946a731aa5b0e5202e0bd594506
s-endo17 <yattsa0902@gmail.com>
2022-05-31 14:19:01 +0900

    fix: resolve import error

--- front/utils/__init__.py
+++ front/utils/__init__.py
@@ -1,6 +1,5 @@
 from lib.webrtc_ui.calculate_fps import *
 from lib.webrtc_ui.calib_cam import *
-from utils.class_objects import *
 from lib.pose.draw_pose import *
 from lib.webrtc_ui.webcam_input import *
 from utils.multiapp import *
