16c36c9db7a60c384f982dd7dec76ff3868db433
s-endo17 <yattsa0902@gmail.com>
2022-06-21 11:13:33 +0900

    chore: upload react-sandbox

--- front/electron/src/renderer/App.tsx
+++ front/electron/src/renderer/App.tsx
@@ -1,49 +1,13 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
-import icon from '../../assets/icon.svg';
 import './App.css';
-
-const Hello = () => {
-  return (
-    <div>
-      <div className="Hello">
-        <img width="200px" alt="icon" src={icon} />
-      </div>
-      <h1 style={{ textAlign: 'center' }}>welcome to autofit</h1>
-      <div className="Hello">
-        <a
-          href="https://github.com/autofit-hackers/autofit"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://startingstrength.com/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              💪
-            </span>
-            do training
-          </button>
-        </a>
-      </div>
-    </div>
-  );
-};
+import SampleComponent from './report';
+import Welcome from './welcomeTemplatePage';
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<SampleComponent />} />
       </Routes>
     </Router>
   );
