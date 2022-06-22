b6739595d7bab9fc82d62d3020466ca6cfdc8433
s-endo17 <yattsa0902@gmail.com>
2022-06-22 22:41:07 +0900

    feat: add report first prototype

--- front/electron/src/renderer/App.tsx
+++ front/electron/src/renderer/App.tsx
@@ -1,19 +1,16 @@
 // import Dashboard from './mui_template/Dashboard';
 import { Button } from '@mui/material';
 import Dashboard from './mui_template/Dashboard';
-import { Hoge, PropTest, ReportSample } from './report';
+import { IntervalReport, PropTest } from './report';
 
 export default function App() {
   return (
     <>
-      {/* <Hoge
-        names={{
-          lastName: 'Endo',
-          firstName: 'Satoshi',
-        }}
-      /> */}
-      <PropTest name="ss" num={1} />
-      <Dashboard />
+      <IntervalReport
+        trainingMenuName="スクワット"
+        frontMoviePath="/aa"
+        instructionText="膝が内に入っていますね"
+      />
     </>
   );
 }
--- front/electron/src/renderer/report.tsx
+++ front/electron/src/renderer/report.tsx
@@ -1,35 +1,118 @@
-import { Button, Typography } from '@mui/material';
+import { Copyright, Headphones } from '@mui/icons-material';
+import {
+  AppBar,
+  Badge,
+  Box,
+  Button,
+  CardMedia,
+  CssBaseline,
+  Divider,
+  Drawer,
+  Grid,
+  IconButton,
+  List,
+  Paper,
+  ThemeProvider,
+  Toolbar,
+  Typography,
+} from '@mui/material';
+import { Container } from '@mui/system';
 import PropTypes from 'prop-types';
+import BarChart from './mui_template/Chart';
 
-export function ReportSample(): JSX.Element {
-  return (
-    <Button variant="contained" color="primary">
-      props ={' '}
-    </Button>
-  );
+interface IntervalReportProps {
+  trainingMenuName: string;
+  frontMoviePath: string;
+  instructionText: string;
 }
 
-export const Hoge = () => {
-  const f = 'aa';
-  const l = 'AA';
-  // { names: { lastName: l, firstName: f } }
+export const IntervalReport = (prop: IntervalReportProps) => {
+  const { trainingMenuName: tn, frontMoviePath, instructionText: inst } = prop;
   return (
-    <Typography variant="h4" fontWeight={600}>
-      僕の名前は {f} {l} です！
-    </Typography>
+    <>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+          }}
+        >
+          <Toolbar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            <Grid container spacing={3}>
+              {/* Chart */}
+              <Grid item xs={12} md={8} lg={9}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                  }}
+                >
+                  <BarChart />
+                </Paper>
+              </Grid>
+              {/* Recent Deposits */}
+              <Grid item xs={12} md={4} lg={3}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: 240,
+                  }}
+                />
+              </Grid>
+              {/* Recent Deposits */}
+              <Grid item xs={12} md={8} lg={9}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                  }}
+                >
+                  <Headphones />
+                  <Typography variant="h4" fontWeight={600}>
+                    {tn} おつかれさまでした。
+                  </Typography>
+                  <CardMedia
+                    sx={{ borderRadius: 10 }}
+                    component="iframe"
+                    src="https://www.youtube.com/embed/muuK4SpRR5M"
+                  />
+                  <Typography variant="h4" fontWeight={600}>
+                    {inst}
+                  </Typography>
+                </Paper>
+              </Grid>
+            </Grid>
+          </Container>
+        </Box>
+      </Box>
+    </>
   );
 };
 
 interface Props {
   name: string;
-  num: number;
+  age: number;
 }
 
-export const PropTest: React.FC<Props> = ({ name: n, num: v }: Props) => {
+export const PropTest = (prop: Props) => {
+  const { name, age } = prop;
   return (
     <>
       <Typography variant="h4" fontWeight={600}>
-        僕の名前は {n} {v} です！
+        僕の名前は {name}、 {age} 歳です！
       </Typography>
     </>
   );
