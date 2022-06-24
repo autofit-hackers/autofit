4717d63a3cc291511781495cf7908fa7eff1b26e
s-endo17 <53333254+s-endo17@users.noreply.github.com>
2022-06-24 21:55:02 +0900

    Revert "Revert "Autofit 105 1 セット終了後のレポートを表示する""

--- front/electron/src/renderer/report.tsx
+++ front/electron/src/renderer/report.tsx
@@ -1,24 +1,115 @@
-import { Stack, Button } from '@mui/material';
-import theme from './theme';
+import {
+  Box,
+  CardMedia,
+  CssBaseline,
+  Grid,
+  Paper,
+  Typography,
+} from '@mui/material';
+import { Container } from '@mui/system';
+import SearchAppBar from './AppBar';
+import BarChart from './Chart';
 
-export default function SampleComponent(): JSX.Element {
+interface IntervalReportProps {
+  trainingMenuName: string;
+  frontMoviePath: string;
+  instructionText: string;
+}
+
+export default function IntervalReport(prop: IntervalReportProps) {
+  const {
+    trainingMenuName: tn,
+    frontMoviePath: fvpath,
+    instructionText: inst,
+  } = prop;
   return (
-    <Stack direction="row" spacing={2} sx={{ m: 2, p: 2 }}>
-      <Button variant="contained" color="primary">
-        primary
-      </Button>
-      <Button variant="contained" color="secondary">
-        secondary
-      </Button>
-      <Button variant="contained" color="warning">
-        warning
-      </Button>
-      <Button variant="contained" color="info">
-        info
-      </Button>
-      <Button variant="contained" color="success">
-        success
-      </Button>
-    </Stack>
+    <>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+          }}
+        >
+          <SearchAppBar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+            <Grid container spacing={3}>
+              {/* text instruction */}
+              <Grid item xs={9}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: '20vh',
+                  }}
+                >
+                  <Typography variant="h4" fontWeight={600}>
+                    {tn} おつかれさまでした。
+                  </Typography>
+                  <Typography variant="h4" fontWeight={600}>
+                    {inst}
+                  </Typography>
+                </Paper>
+              </Grid>
+              {/* text instruction */}
+              <Grid item xs={3}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: '20vh',
+                  }}
+                >
+                  <Typography variant="h4" fontWeight={600}>
+                    Timer
+                  </Typography>
+                </Paper>
+              </Grid>
+              {/* Chart */}
+              <Grid item xs={6}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: '40vh',
+                  }}
+                >
+                  <BarChart />
+                </Paper>
+              </Grid>
+
+              {/* video */}
+              <Grid item xs={6}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: '40vh',
+                  }}
+                >
+                  <CardMedia
+                    sx={{ borderRadius: 3, height: 400 }}
+                    component="iframe"
+                    src={fvpath}
+                  />
+                </Paper>
+              </Grid>
+            </Grid>
+          </Container>
+        </Box>
+      </Box>
+    </>
   );
 }
