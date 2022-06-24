324c05473ba70c8d5a1d9437a4873b1022f4f1ae
s-endo17 <yattsa0902@gmail.com>
2022-06-24 21:22:32 +0900

    refactor: rename files

--- front/electron/src/renderer/App.tsx
+++ front/electron/src/renderer/App.tsx
@@ -1,33 +1,19 @@
-import { MemoryRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Typography } from '@mui/material';
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
 import LabelBottomNavigation from './BottomNavigation';
+import EndoWorkSpace from './Endo';
 import IntervalReport from './Report';
-
-const Navigation = () => {
-  return (
-    <nav>
-      <li>
-        <Link to="report">Report</Link>
-      </li>
-    </nav>
-  );
-};
 
 export default function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Navigation />} />
-          <Route
-            path="/report"
-            element={
-              <IntervalReport
-                trainingMenuName="スクワット"
-                frontMoviePath="https://www.youtube.com/embed/muuK4SpRR5M"
-                instructionText="膝が前に出ています"
-              />
-            }
-          />
+          <Route path="/" element={<Typography>Main</Typography>} />
+          <Route path="/endo" element={<EndoWorkSpace />} />
+          <Route path="/kondo" element={<Typography>KONDO</Typography>} />
+          <Route path="/ueno" element={<Typography>UENO</Typography>} />
+          <Route path="/katsura" element={<Typography>KATSURA</Typography>} />
         </Routes>
         <LabelBottomNavigation />
       </Router>
