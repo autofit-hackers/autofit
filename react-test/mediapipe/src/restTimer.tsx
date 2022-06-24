d0597975e3786baccb12538fbb5067283e57b4a0
katuratt <katuratt@gmail.com>
2022-06-24 23:21:48 +0900

    feat: Not repeat

--- react-test/mediapipe/src/restTimer.tsx
+++ react-test/mediapipe/src/restTimer.tsx
@@ -1,45 +1,23 @@
 import React, {useState } from "react";
 import "./App.css";
-import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import SignIn from "./SignIn";
-import PoseEstimation from "./poseEstimation";
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import { grey, orange, purple } from "@material-ui/core/colors";
-import { Container } from "@material-ui/core";
-import { useTimer } from "react-timer-hook";
-
-import ReactDOM from "react-dom";
 import { CountdownCircleTimer } from 'react-countdown-circle-timer'
 
-
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
-import { Doughnut } from 'react-chartjs-2';
 ChartJS.register(ArcElement, Tooltip, Legend);
-
 
 
 /*レスト時間の指定*/
 const restTime = 10;
-/*レスト時間延長の設定*/
-const restExtension = 5;
-
 
 
 const renderTime = ({remainingTime}: {remainingTime: any}) => {
   if (remainingTime === 0) {
-    return <div className="timer">Let's start muscle training!</div>;
+    return <div className="timer">Let's start <br /> muscle training!</div>;
   }
   return (
     <div className="timer">
-    {/* <div className="text">Remaining</div> */}
-      <div style={{ fontSize: "40px" }}>{remainingTime}</div>
-      <div className="text">seconds</div>
+      <div style={{ fontSize: "50px" }}>{remainingTime}</div>
+      <div>seconds</div>
     </div>
   );
 };
@@ -66,6 +44,5 @@ function RestTimers() {
     </div>
   );
 }
-
 
 export default RestTimers;
