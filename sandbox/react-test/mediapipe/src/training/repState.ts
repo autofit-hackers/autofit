afe0b28051b96d313ac57412cd075dc15b6a3625
masaueno <ueno-masaki202@g.ecc.u-tokyo.ac.jp>
2022-07-02 21:18:10 +0900

    AUTOFIT-116 refactor: change class and methods to type and funcs

--- sandbox/react-test/mediapipe/src/training/repState.ts
+++ sandbox/react-test/mediapipe/src/training/repState.ts
@@ -1,75 +1,84 @@
 import Pose from "./pose";
 
-class RepState {
-    repCount: number = 0;
-    isLiftingUp: boolean = false;
-    didTouchBottom: boolean = false;
-    didTouchTop: boolean = true;
-    initialBodyHeight: number = 0;
-    tmpBodyHeights: number[] = [];
+export type RepState = {
+    repCount: number;
+    isLiftingUp: boolean;
+    didTouchBottom: boolean;
+    didTouchTop: boolean;
+    initialBodyHeight: number;
+    tmpBodyHeights: number[];
+};
 
-    private initRep = (height: number) => {
-        this.initialBodyHeight = height;
-        this.tmpBodyHeights = new Array<number>(10);
-        for (let i = 0; i < 10; i++) {
-            this.tmpBodyHeights[i] = this.initialBodyHeight;
-        }
-    };
-
-    updateRepCount = (pose: Pose, lowerThreshold: number, upperThreshold: number) => {
-        const height: number = pose.height();
-        if (this.tmpBodyHeights.length < 10) {
-            this.initRep(height);
-        }
-        const has_count_upped = this.checkIfRepFinished(height, lowerThreshold, upperThreshold);
-        this.updateLiftingState(height);
-        return has_count_upped;
-    };
-
-    private checkIfRepFinished = (
-        height: number,
-        lowerThreshold: number,
-        upperThreshold: number,
-    ): boolean => {
-        if (!this.didTouchBottom && height < this.initialBodyHeight * lowerThreshold) {
-            this.didTouchBottom = true;
-        } else if (this.didTouchBottom && height > this.initialBodyHeight * upperThreshold) {
-            this.repCount += 1;
-            this.didTouchBottom = false;
-            return true;
-        }
-        return false;
-    };
-
-    private updateLiftingState = (height: number): void => {
-        this.tmpBodyHeights.shift();
-        this.tmpBodyHeights.push(height);
-    };
-
-    isKeyframe = (pose: Pose, lowerThreshold: number, upperThreshold: number) => {
-        const height: number = pose.height();
-        if (this.didTouchTop && height < this.initialBodyHeight * lowerThreshold) {
-            this.didTouchTop = false;
-            return true;
-        } else if (!this.didTouchTop && height > this.initialBodyHeight * upperThreshold) {
-            this.didTouchTop = true;
-            return false;
-        } else {
-            return false;
-        }
-    };
-
-    // TODO: implement alternative of body_heights_df in training_set.py
-    resetRep(pose: Pose): void {
-        this.repCount = 0;
-        this.isLiftingUp = false;
-        this.didTouchBottom = false;
-        this.didTouchTop = true;
-        this.initialBodyHeight = pose.height();
-        for (let i = 0; i < 10; i++) {
-            this.tmpBodyHeights[i] = this.initialBodyHeight;
-        }
+export const initRep = (repState: RepState, height: number) => {
+    repState.initialBodyHeight = height;
+    repState.tmpBodyHeights = new Array<number>(10);
+    for (let i = 0; i < 10; i++) {
+        repState.tmpBodyHeights[i] = repState.initialBodyHeight;
     }
-}
+};
 
-export default RepState;
+export const updateRepCount = (
+    repState: RepState,
+    pose: Pose,
+    lowerThreshold: number,
+    upperThreshold: number,
+) => {
+    const height: number = pose.height();
+    if (repState.tmpBodyHeights.length < 10) {
+        initRep(repState, height);
+    }
+    const has_count_upped = checkIfRepFinished(repState, height, lowerThreshold, upperThreshold);
+    updateLiftingState(repState, height);
+    return has_count_upped;
+};
+
+export const checkIfRepFinished = (
+    repState: RepState,
+    height: number,
+    lowerThreshold: number,
+    upperThreshold: number,
+): boolean => {
+    if (!repState.didTouchBottom && height < repState.initialBodyHeight * lowerThreshold) {
+        repState.didTouchBottom = true;
+    } else if (repState.didTouchBottom && height > repState.initialBodyHeight * upperThreshold) {
+        repState.repCount += 1;
+        repState.didTouchBottom = false;
+        return true;
+    }
+    return false;
+};
+
+export const updateLiftingState = (repState: RepState, height: number): void => {
+    repState.tmpBodyHeights.shift();
+    repState.tmpBodyHeights.push(height);
+};
+
+export const isKeyframe = (
+    repState: RepState,
+    pose: Pose,
+    lowerThreshold: number,
+    upperThreshold: number,
+) => {
+    const height: number = pose.height();
+    if (repState.didTouchTop && height < repState.initialBodyHeight * lowerThreshold) {
+        repState.didTouchTop = false;
+        return true;
+    } else if (!repState.didTouchTop && height > repState.initialBodyHeight * upperThreshold) {
+        repState.didTouchTop = true;
+        return false;
+    } else {
+        return false;
+    }
+};
+
+// TODO: implement alternative of body_heights_df in training_set.py
+export const resetRep = (repState: RepState, pose: Pose): void => {
+    repState.repCount = 0;
+    repState.isLiftingUp = false;
+    repState.didTouchBottom = false;
+    repState.didTouchTop = true;
+    repState.initialBodyHeight = pose.height();
+    for (let i = 0; i < 10; i++) {
+        repState.tmpBodyHeights[i] = repState.initialBodyHeight;
+    }
+};
