d17be655e10d665c1cd2b300b704862a46a627e4
masaueno <ueno-masaki202@g.ecc.u-tokyo.ac.jp>
2022-07-04 23:35:12 +0900

    AUTOFIT-116 chore: modify gitignore to add html

--- sandbox/react-test/mediapipe/src/index.tsx
+++ sandbox/react-test/mediapipe/src/index.tsx
@@ -4,7 +4,9 @@ import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(
+    document.getElementById("root") as HTMLElement,
+);
 root.render(
     <React.StrictMode>
         <App />
