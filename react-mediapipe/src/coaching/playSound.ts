06bb962cdbbc1f011fc4322f8559aa534f85a901
masaueno <ueno-masaki202@g.ecc.u-tokyo.ac.jp>
2022-07-11 20:01:38 +0900

    AUTOFIT-122 feat: play rep count sound

---
+++ react-mediapipe/src/coaching/playSound.ts
@@ -0,0 +1,12 @@
+export const playRepCountSound = (repCount: number): void => {
+    // ブラウザにWeb Speech API Speech Synthesis機能があるか判定
+    if ('speechSynthesis' in window) {
+        // 発言を設定
+        const uttr = new SpeechSynthesisUtterance();
+        uttr.text = `${repCount}`;
+        // 発言を再生
+        window.speechSynthesis.speak(uttr);
+    } else {
+        alert('このブラウザは音声合成に対応していません。');
+    }
+};
