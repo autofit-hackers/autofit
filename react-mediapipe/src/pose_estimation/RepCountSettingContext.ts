f010160d5374289bac63e42da3d407e2f242d8fc
masaueno <ueno-masaki202@g.ecc.u-tokyo.ac.jp>
2022-07-20 15:42:23 +0900

    AUTOFIT-124 refactor: delete deprecated components

--- react-mediapipe/src/pose_estimation/RepCountSettingContext.ts
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react';
-
-// settingsの初期値を定義
-export type RepCountSetting = { upperThreshold: number; lowerThreshold: number };
-export const defaultRepCountSettings: RepCountSetting = { upperThreshold: 0.8, lowerThreshold: 0.9 };
-
-// settingsをcontextにして下位コンポーネント(PoseStream)で使用可能にする
-export const RepCountSettingContext = createContext<RepCountSetting>(defaultRepCountSettings);
