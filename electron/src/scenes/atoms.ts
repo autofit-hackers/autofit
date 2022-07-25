b1bc7206247203d4918c01c73c2cf52e78e69617
masaueno <93699483+masaueno@users.noreply.github.com>
2022-07-26 01:26:46 +0900

    AUTOFIT-133 feat: stop kinect process in set report (#75)
    * AUTOFIT-133 feat: stop kinect process in set report
    
    * fix: delete one of error logs when try to stop kinect
    
    * AUTOFIT-133 fix: delete one of error logs when try to stop kinect

--- electron/src/scenes/atoms.ts
+++ electron/src/scenes/atoms.ts
@@ -1,7 +1,15 @@
 import { atom } from 'jotai';
 import { Set } from '../training/set';
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
+const KinectAzure = require('kinect-azure');
+
 export const phaseAtom = atom<number>(0);
 
 export const repVideoUrlsAtom = atom<string[]>([]);
 export const setRecordAtom = atom<Set>({ reps: [] });
+
+// KinectAzure
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+const kinect = new KinectAzure();
+export const kinectAtom = atom<typeof KinectAzure>(kinect);
