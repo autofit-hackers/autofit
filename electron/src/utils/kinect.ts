8d041339a47ad2083b0bcee7eafcadae719f3084
masaueno <ueno-masaki202@g.ecc.u-tokyo.ac.jp>
2022-08-06 01:30:15 +0900

    AUTOFIT-134 chore: delete gpu specification

--- electron/src/utils/kinect.ts
+++ electron/src/utils/kinect.ts
@@ -30,7 +30,6 @@ export const startKinect = (
     kinect.createTracker({
       // TODO: use GPU if available otherwise use CPU
       processing_mode: KinectAzure.K4ABT_TRACKER_PROCESSING_MODE_GPU_CUDA,
-      gpu_device_id: 0,
     });
     kinect.startListening(onResults);
   }
