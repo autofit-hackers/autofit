2310e115f1d435c834f5e8834be8f7c4c93ab2af
masaueno <ueno-masaki202@g.ecc.u-tokyo.ac.jp>
2022-08-09 17:39:05 +0900

    feat: replay worst rep video according to selected instruction

--- electron/src/scenes/Report.tsx
+++ electron/src/scenes/Report.tsx
@@ -1,32 +1,22 @@
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import LocationOnIcon from '@mui/icons-material/LocationOn';
 import RestoreIcon from '@mui/icons-material/Restore';
 import { BottomNavigation, BottomNavigationAction, Box, createTheme, CssBaseline, Grid, Paper } from '@mui/material';
 import { Container, ThemeProvider } from '@mui/system';
 import { useAtom } from 'jotai';
-import { useState } from 'react';
-import { formInstructionItems } from '../coaching/formInstructionItems';
-import { repVideoUrlsAtom, setRecordAtom } from './atoms';
+import { useEffect, useState } from 'react';
+import { recordFromEvaluationResult } from '../coaching/formInstruction';
+import { formInstructionItemsAtom, setRecordAtom } from './atoms';
 import { GoodPoint, VideoReplayer } from './ReportComponents';
 
 export default function IntervalReport() {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [repVideoUrls] = useAtom(repVideoUrlsAtom);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const [repIndexToShow, setValue] = useState(0);
-  const [set] = useAtom(setRecordAtom);
-  const [displayingInstructionIdx, setIdx] = useState(0);
+  const [setRecord, setSetRecord] = useAtom(setRecordAtom);
+  const [formInstructionItems] = useAtom(formInstructionItemsAtom);
+  const [selectedInstructionIndex, setSelectedInstructionIndex] = useState(0);
+  const displayedRepIndex = setRecord.formEvaluationResults[selectedInstructionIndex].worstRepIndex;
 
-  const repScore: { [key: string]: number } = {};
-  const instructionKeys: string[] = [];
-  console.log(set);
-  set.reps.forEach((rep) => {
-    Object.keys(rep.formErrors).forEach((key) => {
-      repScore[key] = rep.formErrors[key];
-      instructionKeys.push(key);
-      console.log('pushed: ', key);
-    });
-  });
+  // Reportコンポーネントマウント時にセット変数にフォーム分析結果を記録する
+  useEffect(() => {
+    setSetRecord(recordFromEvaluationResult(setRecord, formInstructionItems));
+  }, [formInstructionItems, setRecord, setSetRecord]);
 
   const futuristicTheme = createTheme({
     palette: {
@@ -58,8 +48,8 @@ export default function IntervalReport() {
       >
         <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
           <Grid container spacing="1vh">
-            {/* 撮影した16:9の動画 */}
-            <VideoReplayer />
+            {/* 撮影したRGB映像 */}
+            <VideoReplayer displayedRepIndex={displayedRepIndex} />
             {/* トレーニングの3D表示 */}
             <Grid item xs={12}>
               <Paper
@@ -71,14 +61,12 @@ export default function IntervalReport() {
                 }}
               />
             </Grid>
-            <GoodPoint
-              text={formInstructionItems[instructionKeys[displayingInstructionIdx]].instructionText ?? 'null'}
-            />
+            <GoodPoint text={formInstructionItems[selectedInstructionIndex].text ?? 'null'} />
             <BottomNavigation
               showLabels
-              value={displayingInstructionIdx}
+              value={selectedInstructionIndex}
               onChange={(event, newValue: number) => {
-                setIdx(newValue);
+                setSelectedInstructionIndex(newValue);
               }}
               sx={{
                 mx: 'auto',
@@ -86,86 +74,25 @@ export default function IntervalReport() {
                 '& .Mui-selected': { backgroundColor: '#005555' },
               }}
             >
-              {/* TODO: ループ処理にまとめる */}
-              <BottomNavigationAction
-                label={formInstructionItems[instructionKeys[0]].instructionTitle}
-                icon={<RestoreIcon />}
-                sx={{
-                  backgroundColor: 'grey.900',
-                  borderRadius: 0,
-                  border: 1,
-                  borderColor: 'grey.500',
-                  borderTop: 0,
-                  borderTopColor: '#006666',
-                  borderBottomRightRadius: 20,
-                  borderBottomLeftRadius: 20,
-                  boxShadow: 0,
-                  mr: 5,
-                }}
-              />
-              <BottomNavigationAction
-                label={formInstructionItems[instructionKeys[1]].instructionTitle}
-                icon={<FavoriteIcon />}
-                sx={{
-                  backgroundColor: 'grey.900',
-                  borderRadius: 0,
-                  border: 1,
-                  borderColor: 'grey.500',
-                  borderTop: 0,
-                  borderTopColor: 'grey.900',
-                  borderBottomRightRadius: 20,
-                  borderBottomLeftRadius: 20,
-                  boxShadow: 0,
-                  mr: 5,
-                }}
-              />
-              <BottomNavigationAction
-                label={formInstructionItems[instructionKeys[2]].instructionTitle}
-                icon={<LocationOnIcon />}
-                sx={{
-                  backgroundColor: 'grey.900',
-                  borderRadius: 0,
-                  border: 1,
-                  borderColor: 'grey.500',
-                  borderTop: 0,
-                  borderTopColor: 'grey.900',
-                  borderBottomRightRadius: 20,
-                  borderBottomLeftRadius: 20,
-                  boxShadow: 0,
-                  mr: 5,
-                }}
-              />
-              <BottomNavigationAction
-                label={formInstructionItems[instructionKeys[3]].instructionTitle}
-                icon={<FavoriteIcon />}
-                sx={{
-                  backgroundColor: 'grey.900',
-                  borderRadius: 0,
-                  border: 1,
-                  borderColor: 'grey.500',
-                  borderTop: 0,
-                  borderTopColor: 'grey.900',
-                  borderBottomRightRadius: 20,
-                  borderBottomLeftRadius: 20,
-                  boxShadow: 0,
-                  mr: 5,
-                }}
-              />
-              <BottomNavigationAction
-                label={formInstructionItems[instructionKeys[4]].instructionTitle}
-                icon={<LocationOnIcon />}
-                sx={{
-                  backgroundColor: 'grey.900',
-                  borderRadius: 0,
-                  border: 1,
-                  borderColor: 'grey.500',
-                  borderTop: 0,
-                  borderTopColor: 'grey.900',
-                  borderBottomRightRadius: 20,
-                  borderBottomLeftRadius: 20,
-                  boxShadow: 0,
-                }}
-              />
+              {/* TODO: コンポーネントのマウント時にタブが表示されない問題を解決する。おそらくselectedをindex0でtrueに設定すればよい。 */}
+              {formInstructionItems.map((instructionItem) => (
+                <BottomNavigationAction
+                  label={instructionItem.label}
+                  icon={<RestoreIcon />}
+                  sx={{
+                    backgroundColor: 'grey.900',
+                    borderRadius: 0,
+                    border: 1,
+                    borderColor: 'grey.500',
+                    borderTop: 0,
+                    borderTopColor: '#006666',
+                    borderBottomRightRadius: 20,
+                    borderBottomLeftRadius: 20,
+                    boxShadow: 0,
+                    mr: 5,
+                  }}
+                />
+              ))}
             </BottomNavigation>
 
             {/* 詳細表示する指導の切り替えボタン類 */}
