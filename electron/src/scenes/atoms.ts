8b407fe98ac07f125da3fbb692c39ea2bcc4da93
masaueno <ueno-masaki202@g.ecc.u-tokyo.ac.jp>
2022-08-10 10:40:31 +0900

    Revert "Revert "refactor: move form instruction function""
    This reverts commit 5fe612f75c24fd6e8dbfbfae13139147e740790d.

--- electron/src/scenes/atoms.ts
+++ electron/src/scenes/atoms.ts
@@ -1,5 +1,6 @@
 import { atom } from 'jotai';
-import { FormInstructionItem, formInstructionItemsQWS } from '../coaching/formInstructionItems';
+import { FormInstructionItem } from '../coaching/formInstruction';
+import { formInstructionItemsQWS } from '../coaching/formInstructionItems';
 import { resetSet, Set } from '../training/set';
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
 const KinectAzure = require('kinect-azure');
