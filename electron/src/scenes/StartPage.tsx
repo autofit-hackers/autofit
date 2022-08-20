b87b0e0ab07ac32ab5301af6cb3298abaf7e42f7
masaueno <ueno-masaki202@g.ecc.u-tokyo.ac.jp>
2022-08-20 12:55:26 +0900

    refactor: delete unnecessary css property

--- electron/src/scenes/StartPage.tsx
+++ electron/src/scenes/StartPage.tsx
@@ -22,7 +22,7 @@ export default function StartPage() {
           height: '100%',
         }}
       >
-        <AutofitLogo scale={1.3} style={{ display: 'block', margin: '50px' }} />
+        <AutofitLogo scale={1.3} style={{ margin: '50px' }} />
         <button
           type="button"
           className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800"
