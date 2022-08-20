e6fd3d2b1998d5bb9efa0d827a661e1f6d845e7e
masaueno <ueno-masaki202@g.ecc.u-tokyo.ac.jp>
2022-08-20 12:15:31 +0900

    feat: gradient color background

---
+++ electron/src/scenes/StartPage.tsx
@@ -0,0 +1,34 @@
+import { useAtom } from 'jotai';
+import { phaseAtom } from './atoms';
+import './StartPage.css';
+
+export default function StartPage() {
+  const [, setPhase] = useAtom(phaseAtom);
+  const handleStart = () => {
+    setPhase(1);
+  };
+
+  return (
+    <div className="gradient-background" style={{ height: '100vh', width: '100vw' }}>
+      <div
+        style={{
+          // display: 'flex',
+          // textAlign: 'center',
+          // justifyContent: 'center',
+          // alignItems: 'center',
+          width: '100vw',
+          height: '100%',
+        }}
+      >
+        <button
+          type="button"
+          className="text-white bg-gradient-to-r from-purple-500 to-pink-500 hover:bg-gradient-to-l focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"
+          onClick={handleStart}
+          style={{}}
+        >
+          Start Training with autofit!
+        </button>
+      </div>
+    </div>
+  );
+}
