5f4551c9994a9877618fb2b2e50846c3dc166571
Yusuke Kondo <kondounagi@gmail.com>
2022-09-03 21:18:47 +0900

    chore: add interval report

--- electron/src/scenes/Main.tsx
+++ electron/src/scenes/Main.tsx
@@ -3,6 +3,7 @@ import { ThemeProvider } from '@mui/system';
 import { useAtom } from 'jotai';
 import { phaseAtom } from './atoms';
 import BodyTrack2d from './BodyTracking';
+import IntervalReport from './IntervalReport';
 import StartPage from './StartPage';
 import TakeoutReports from './TakeOutReports';
 import futuristicTheme from './themes';
@@ -17,7 +18,8 @@ export default function TrainingMain() {
       <ProductMenu />
       {phase === 0 && <StartPage />}
       {phase === 1 && <BodyTrack2d />}
-      {phase === 2 && <TakeoutReports />}
+      {phase === 2 && <IntervalReport />}
+      {phase === 3 && <TakeoutReports />}
     </ThemeProvider>
   );
 }
--- electron/src/scenes/TakeOutReports.tsx
+++ electron/src/scenes/TakeOutReports.tsx
@@ -1,23 +1,29 @@
-import { Button } from '@mui/material';
-import { useRef } from 'react';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import { IconButton } from '@mui/material';
+import { useAtom } from 'jotai';
+import { phaseAtom } from './atoms';
 import TakeoutReport1 from './TakeoutReport1';
 import TakeoutReport2 from './TakeoutReport2';
 import TakeoutReport3 from './TakeoutReport3';
 
 function TakeoutReports() {
-  // react-to-print
-  const componentToPrintRef = useRef<HTMLDivElement>(null);
+  const [, setPhase] = useAtom(phaseAtom);
 
   return (
     <div>
-      {/* You have to place ReactToPrint component as a sibling of the component you want to print. */}
-      {/* <ReactToPrint trigger={() => PrintButton()} content={() => componentToPrintRef.current} print={handlePrint} /> */}
-      <Button onClick={() => window.print()}> print this out</Button>
-      <div ref={componentToPrintRef}>
-        <TakeoutReport1 />
-        <TakeoutReport2 />
-        <TakeoutReport3 />
-      </div>
+      <IconButton
+        aria-label="reset-camera-angle"
+        color="primary"
+        onClick={() => {
+          setPhase(1);
+        }}
+        sx={{ zIndex: 2 }}
+      >
+        <CheckCircleIcon />
+      </IconButton>
+      <TakeoutReport1 />
+      <TakeoutReport2 />
+      <TakeoutReport3 />
     </div>
   );
 }
