53d3655876d8903cd1740a18bcef3da0b91b2438
Yusuke Kondo <kondounagi@gmail.com>
2022-09-03 17:53:45 +0900

    chore: tmp commit

--- electron/src/scenes/TakeOutReports.tsx
+++ electron/src/scenes/TakeOutReports.tsx
@@ -1,15 +1,43 @@
+import { Button } from '@mui/material';
+import { useRef } from 'react';
 import TakeoutReport1 from './TakeoutReport1';
 import TakeoutReport2 from './TakeoutReport2';
 import TakeoutReport3 from './TakeoutReport3';
 
-function TakeoutReport() {
+function TakeoutReports() {
+  // react-to-print
+  const componentToPrintRef = useRef<HTMLDivElement>(null);
+
+  // const handlePrint = (target) =>
+  //   new Promise(() => {
+  //     console.log('forwarding print request to the main process...');
+
+  //     // convert the iframe into data url
+  //     // https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URIs
+  //     const data = target.contentWindow.document.documentElement.outerHTML;
+  //     // console.log(data);
+  //     const blob = new Blob([data], { type: 'text/html' });
+  //     const url = URL.createObjectURL(blob);
+
+  //     console.log(url);
+
+  //     window.electronAPI.printComponent(url, (response) => {
+  //       console.log('Main: ', response);
+  //     });
+  //   });
+
   return (
-    <>
-      <TakeoutReport1 />
-      <TakeoutReport2 />
-      <TakeoutReport3 />
-    </>
+    <div>
+      {/* You have to place ReactToPrint component as a sibling of the component you want to print. */}
+      {/* <ReactToPrint trigger={() => PrintButton()} content={() => componentToPrintRef.current} print={handlePrint} /> */}
+      <Button onClick={() => window.print()}> print this out</Button>
+      <div ref={componentToPrintRef}>
+        <TakeoutReport1 />
+        <TakeoutReport2 />
+        <TakeoutReport3 />
+      </div>
+    </div>
   );
 }
 
-export default TakeoutReport;
+export default TakeoutReports;
