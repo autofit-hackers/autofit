15e838aa584067a2014488c996621b74cde6dc6f
Yusuke KONDO <kondounagi@gmail.com>
2022-10-13 00:25:34 +0900

    feat: page transition with fade-in and fade-out powered by framer-motion

--- electron/src/scenes/Main.tsx
+++ electron/src/scenes/Main.tsx
@@ -3,8 +3,10 @@ import { ThemeProvider } from '@mui/system';
 import { useAtom } from 'jotai';
 import { phaseAtom } from './atoms';
 import BodyTracking from './BodyTracking';
+import FadeInOut from './decorators/FadeInOut';
 import Report1 from './Report1';
 import Report2 from './Report2';
+import StartPage from './StartPage';
 import futuristicTheme from './themes';
 import ProductMenu from './ui-components/ProductMenu';
 
@@ -15,9 +17,26 @@ export default function TrainingMain() {
     <ThemeProvider theme={futuristicTheme}>
       <CssBaseline />
       <ProductMenu />
-      {phase === 0 && <BodyTracking />}
-      {phase === 1 && <Report1 />}
-      {phase === 2 && <Report2 />}
+      {phase === 0 && (
+        <FadeInOut>
+          <StartPage />
+        </FadeInOut>
+      )}
+      {phase === 1 && (
+        <FadeInOut>
+          <BodyTracking />
+        </FadeInOut>
+      )}
+      {phase === 2 && (
+        <FadeInOut>
+          <Report1 />
+        </FadeInOut>
+      )}
+      {phase === 3 && (
+        <FadeInOut>
+          <Report2 />
+        </FadeInOut>
+      )}
     </ThemeProvider>
   );
 }
