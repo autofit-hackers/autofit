5d337880db8b508100ed46fb8224d098eee63358
Satoshi Endo <53333254+s-endo17@users.noreply.github.com>
2022-10-12 21:44:30 +0900

    feat: add result (#130)

--- electron/src/scenes/atoms.ts
+++ electron/src/scenes/atoms.ts
@@ -1,15 +1,15 @@
 import { atom } from 'jotai';
+import { FormInstructionItem } from '../coaching/formInstruction';
 import kneeFrontAndBack from '../coaching/squat-form-instructions/kneeFrontAndBack';
 import kneeInAndOut from '../coaching/squat-form-instructions/kneeInAndOut';
 import squatDepth from '../coaching/squat-form-instructions/squatDepth';
-import stanceWidth from '../coaching/squat-form-instructions/stanceWidth';
 import squatVelocity from '../coaching/squat-form-instructions/squatVelocity';
-import { FormInstructionItem } from '../coaching/formInstruction';
+import stanceWidth from '../coaching/squat-form-instructions/stanceWidth';
 import { resetSet, Set } from '../training_data/set';
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-var-requires
 const KinectAzure = require('kinect-azure');
 
-export const phaseAtom = atom<number>(1);
+export const phaseAtom = atom<number>(4);
 
 export const formInstructionItemsAtom = atom<FormInstructionItem[]>([
   squatDepth,
