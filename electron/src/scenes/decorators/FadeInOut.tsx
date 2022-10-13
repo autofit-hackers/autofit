6bd59b4a1e30c840fc82e73f56dba2bb4537d3cd
Yusuke Kondo <38452345+kondounagi@users.noreply.github.com>
2022-10-13 10:17:22 +0900

    feat: page transition with fade-in and fade-out powered by framer-motion (#132)

---
+++ electron/src/scenes/decorators/FadeInOut.tsx
@@ -0,0 +1,10 @@
+import { motion } from 'framer-motion';
+import { ReactNode } from 'react';
+
+export default function FadeInOut({ children, duration = 1.0 }: { children: ReactNode; duration: number }) {
+  return (
+    <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }} transition={{ duration }}>
+      {children}
+    </motion.div>
+  );
+}
