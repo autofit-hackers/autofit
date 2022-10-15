2be728b9bb999ec807ad0fddf8d85cf65ee8addb
s-endo17 <yattsa0902@gmail.com>
2022-10-15 13:26:27 +0900

    feat: add random loading message

--- electron/src/scenes/LoadingScreen.tsx
+++ electron/src/scenes/LoadingScreen.tsx
@@ -1,10 +1,20 @@
 import { CircularProgress, Stack, Typography } from '@mui/material';
 
 export default function LoadingScreen() {
+  const randomNumberInRange = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min;
+  const randomLoadingMessage = [
+    'データを読み込んでいます0',
+    'データを読み込んでいます1',
+    'データを読み込んでいます2',
+    'データを読み込んでいます3',
+    'データを読み込んでいます4',
+    'データを読み込んでいます5',
+  ];
+
   return (
     <Stack alignItems="center" spacing={4}>
       <CircularProgress color="inherit" />
-      <Typography>【ランダム】スクワットっていいんですよ？</Typography>
+      <Typography>{randomLoadingMessage[randomNumberInRange(0, randomLoadingMessage.length - 1)]}</Typography>
     </Stack>
   );
 }
