d99ced106403a1551fef38f1d3a5213abbe3ea13
s-endo17 <yattsa0902@gmail.com>
2022-10-16 23:44:42 +0900

    change: set objectfit prop of muscle image contain

--- electron/src/scenes/Report1.tsx
+++ electron/src/scenes/Report1.tsx
@@ -1,7 +1,7 @@
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
-import { Grid, Stack, Typography } from '@mui/material';
+import { CardMedia, Grid, Stack, Typography } from '@mui/material';
 import { ReactElement } from 'react';
 import result2 from '../../resources/images/chest.png';
 
@@ -18,8 +18,8 @@ function TextWithIcon(props: { icon: ReactElement; text: string }) {
   );
 }
 
-function ShortResult(props: { icon: ReactElement; text: string }) {
-  const { icon, text } = props;
+function ShortResult(props: { metrics: ReactElement; measuredValue: string }) {
+  const { metrics, measuredValue } = props;
 
   return (
     <Stack
@@ -30,9 +30,9 @@ function ShortResult(props: { icon: ReactElement; text: string }) {
       borderBottom={1}
       sx={{ marginInline: '5vw', mt: '5vh', p: '1vh' }}
     >
-      <TextWithIcon icon={icon} text={text} />
+      {metrics}
       <Typography variant="h5" component="h1" align="left" fontWeight="bold">
-        aaa
+        {measuredValue}
       </Typography>
     </Stack>
   );
@@ -51,14 +51,24 @@ export default function Report1() {
           <Typography variant="h1" component="h1" align="center" borderBottom={1} sx={{ mx: '5vw' }} fontWeight="bold">
             68
           </Typography>
-          <ShortResult text="時間" icon={<AccessTimeIcon fontSize="large" color="primary" />} />
-          <ShortResult text="回数" icon={<FitnessCenterIcon fontSize="large" color="primary" />} />
-          <ShortResult text="消費カロリー" icon={<LocalFireDepartmentIcon fontSize="large" color="primary" />} />
+          <ShortResult
+            metrics={<TextWithIcon icon={<AccessTimeIcon fontSize="large" color="primary" />} text="時間" />}
+            measuredValue="55:35"
+          />
+          <ShortResult
+            metrics={<TextWithIcon icon={<FitnessCenterIcon fontSize="large" color="primary" />} text="回数" />}
+            measuredValue="5回"
+          />
+          <ShortResult
+            metrics={
+              <TextWithIcon icon={<LocalFireDepartmentIcon fontSize="large" color="primary" />} text="消費カロリー" />
+            }
+            measuredValue="92kcal"
+          />
         </Stack>
       </Grid>
       <Grid item xs={6} sx={{ mt: '7vh' }} alignContent="center" justifyContent="center">
-        <img src={result2} alt="Result2" style={{ height: '50vh' }} />
-        {/* <CardMedia component="img" image={result2} alt="Result2" style={{ height: '50vh' }} /> */}
+        <CardMedia component="img" image={result2} alt="Result2" style={{ height: '50vh', objectFit: 'contain' }} />
       </Grid>
     </Grid>
   );
