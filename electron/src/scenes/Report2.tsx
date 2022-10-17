a649ce92df8adc5afed187c180195207419f3cfd
s-endo17 <yattsa0902@gmail.com>
2022-10-17 18:52:51 +0900

    feat: create modal result window

--- electron/src/scenes/Report2.tsx
+++ electron/src/scenes/Report2.tsx
@@ -1,128 +1,125 @@
-import { Box, Button, Chip, Grid, Stack, Typography } from '@mui/material';
+import { Button, Chip, Grid, Modal, Stack, Typography } from '@mui/material';
 import { useAtom } from 'jotai';
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import ReactPlayer from 'react-player';
 // eslint-disable-next-line import/no-unresolved
 import BaseReactPlayer, { BaseReactPlayerProps } from 'react-player/base';
 import { phaseAtom } from './atoms';
-
-export function FlatCard({ children }: { children: React.ReactNode }) {
-  return <Box sx={{ borderRadius: 5, p: 3, borderWidth: 6, borderColor: '#4AC0E3', width: '100%' }}>{children}</Box>;
-}
-
-export function FlatButton({ text, onClick }: { text: string; onClick: () => void }) {
-  return (
-    <Button
-      variant="contained"
-      onClick={onClick}
-      sx={{
-        borderRadius: 20,
-        p: 2,
-        borderWidth: 6,
-        borderColor: '#4AC0E3',
-        fontSize: 'h5.fontSize',
-        fontWeight: 'bold',
-        paddingLeft: '70px',
-        paddingRight: '70px',
-        '&:hover': {
-          backgroundColor: '#4AC0E3',
-          color: 'white',
-        },
-      }}
-    >
-      {text}
-    </Button>
-  );
-}
+import ResultModal from './ResultModal';
+import { FlatButton, FlatCard } from './ui-components/FlatUI';
 
 export default function Report2() {
   const [, setPhase] = useAtom(phaseAtom);
   const videoPlayerRef = useRef<BaseReactPlayer<BaseReactPlayerProps>>(null);
 
+  const [open, setOpen] = useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <Grid container sx={{ paddingBlock: '5vh', paddingInline: '0vw' }}>
-      {/* ヘッダー */}
-      <Grid item xs={12} sx={{ paddingBlock: '2.5vh', paddingInline: '5vw' }}>
-        <Typography variant="h5" component="h1" align="left" borderBottom={1} fontWeight="bold">
-          今回のトレーニング結果
-        </Typography>
-      </Grid>
-      {/* 左側 */}
-      <Grid item xs={6} sx={{ paddingBlock: '2.5vh', paddingInline: '5vw' }}>
-        <ReactPlayer
-          ref={videoPlayerRef}
-          url="../../resources/images/result/sq-video.mov"
-          id="RepVideo"
-          playing
-          loop
-          // controls
-          width="100%"
-          height="100%"
-          style={{
-            borderRadius: '24px',
-            borderColor: '#4AC0E3',
-            borderWidth: '6px',
-            backgroundColor: 'rgba(0, 0, 0, 1.0)',
-          }}
-        />
-      </Grid>
-      {/* 右側 */}
-      <Grid item xs={6} sx={{ paddingBlock: '2.5vh', paddingRight: '5vw' }}>
-        <FlatCard>
-          <Stack spacing={2} alignItems="flex-start">
-            <Chip
-              label="ポイント"
-              sx={{ fontWeight: 'bold', color: 'white', backgroundColor: '#4AC0E3', paddingInline: 1 }}
-            />
-            <Typography variant="h5" component="h1" fontWeight="bold">
-              全体的なフォームはきれいですが、 体幹に力を入れて胴体をまっすぐに保つように。 歌声にもあらわれています。
-            </Typography>
+    <div>
+      <Grid container sx={{ paddingBlock: '5vh', paddingInline: '0vw' }}>
+        {/* ヘッダー */}
+        <Grid item xs={12} sx={{ paddingBlock: '2.5vh', paddingInline: '5vw' }}>
+          <Typography variant="h5" component="h1" align="left" borderBottom={1} fontWeight="bold">
+            今回のトレーニング結果
+          </Typography>
+        </Grid>
+        {/* 左側 */}
+        <Grid item xs={6} sx={{ paddingBlock: '2.5vh', paddingInline: '5vw' }}>
+          <ReactPlayer
+            ref={videoPlayerRef}
+            url="../../resources/images/result/sq-video.mov"
+            id="RepVideo"
+            playing
+            loop
+            // controls
+            width="100%"
+            height="100%"
+            style={{
+              borderRadius: '24px',
+              borderColor: '#4AC0E3',
+              borderWidth: '6px',
+              backgroundColor: 'rgba(0, 0, 0, 1.0)',
+            }}
+          />
+        </Grid>
+        {/* 右側 */}
+        <Grid item xs={6} sx={{ paddingBlock: '2.5vh', paddingRight: '5vw' }}>
+          <FlatCard>
+            <Stack spacing={2} alignItems="flex-start">
+              <Chip
+                label="ポイント"
+                sx={{ fontWeight: 'bold', color: 'white', backgroundColor: '#4AC0E3', paddingInline: 1 }}
+              />
+              <Typography variant="h5" component="h1" fontWeight="bold">
+                全体的なフォームはきれいですが、 体幹に力を入れて胴体をまっすぐに保つように。
+                歌声にもあらわれています。
+              </Typography>
+            </Stack>
+          </FlatCard>
+          {/* 各評価項目のカード */}
+          <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2} sx={{ mt: '5vh' }}>
+            <FlatCard>
+              <Stack spacing={2}>
+                <Chip
+                  label="深さ"
+                  sx={{ fontWeight: 'bold', color: 'white', backgroundColor: '#4AC0E3', paddingInline: 1 }}
+                />
+                <Typography variant="h5" component="h1" fontWeight="bold">
+                  Good
+                </Typography>
+              </Stack>
+            </FlatCard>
+            <FlatCard>
+              <Stack spacing={2}>
+                <Chip
+                  label="速度"
+                  sx={{ fontWeight: 'bold', color: 'white', backgroundColor: '#4AC0E3', paddingInline: 1 }}
+                />
+                <Typography variant="h5" component="h1" fontWeight="bold">
+                  Good
+                </Typography>
+              </Stack>
+            </FlatCard>
+            <FlatCard>
+              <Stack spacing={2}>
+                <Chip
+                  label="姿勢"
+                  sx={{ fontWeight: 'bold', color: 'white', backgroundColor: '#4AC0E3', paddingInline: 1 }}
+                />
+                <Typography variant="h5" component="h1" fontWeight="bold">
+                  Good
+                </Typography>
+              </Stack>
+            </FlatCard>
           </Stack>
-        </FlatCard>
-        {/* 各評価項目のカード */}
-        <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2} sx={{ mt: '5vh' }}>
-          <FlatCard>
-            <Stack spacing={2}>
-              <Chip
-                label="深さ"
-                sx={{ fontWeight: 'bold', color: 'white', backgroundColor: '#4AC0E3', paddingInline: 1 }}
-              />
-              <Typography variant="h5" component="h1" fontWeight="bold">
-                Good
-              </Typography>
-            </Stack>
-          </FlatCard>
-          <FlatCard>
-            <Stack spacing={2}>
-              <Chip
-                label="速度"
-                sx={{ fontWeight: 'bold', color: 'white', backgroundColor: '#4AC0E3', paddingInline: 1 }}
-              />
-              <Typography variant="h5" component="h1" fontWeight="bold">
-                Good
-              </Typography>
-            </Stack>
-          </FlatCard>
-          <FlatCard>
-            <Stack spacing={2}>
-              <Chip
-                label="姿勢"
-                sx={{ fontWeight: 'bold', color: 'white', backgroundColor: '#4AC0E3', paddingInline: 1 }}
-              />
-              <Typography variant="h5" component="h1" fontWeight="bold">
-                Good
-              </Typography>
-            </Stack>
-          </FlatCard>
-        </Stack>
+        </Grid>
+        {/* フッター */}
+        <Grid item xs={12} sx={{ paddingBlock: '2.5vh', paddingInline: '5vw' }}>
+          <Stack direction="row" justifyContent="space-between" alignItems="center" spacing="50vw">
+            <FlatButton text="戻る" onClick={() => setPhase((prev) => prev - 1)} />
+            <FlatButton text="次へ" onClick={() => setPhase((prev) => prev + 1)} />
+          </Stack>
+        </Grid>
       </Grid>
-      {/* フッター */}
-      <Grid item xs={12} sx={{ paddingBlock: '2.5vh', paddingInline: '5vw' }}>
-        <Stack direction="row" justifyContent="space-between" alignItems="center" spacing="50vw">
-          <FlatButton text="戻る" onClick={() => setPhase((prev) => prev - 1)} />
-          <FlatButton text="次へ" onClick={() => setPhase((prev) => prev + 1)} />
-        </Stack>
-      </Grid>
-    </Grid>
+      <Button variant="outlined" color="primary" onClick={handleOpen}>
+        Open modal
+      </Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <ResultModal handleClose={handleClose} />
+      </Modal>
+    </div>
   );
 }
