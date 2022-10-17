0b71ad8a62e5f87f73d890551d2b5a2c52108d95
s-endo17 <yattsa0902@gmail.com>
2022-10-17 16:17:27 +0900

    feat: create coded result2

--- electron/src/scenes/Report2.tsx
+++ electron/src/scenes/Report2.tsx
@@ -1,78 +1,125 @@
-import { Button } from '@mui/material';
-import { Box } from '@mui/system';
+import { Box, Button, Chip, Grid, Stack, Typography } from '@mui/material';
 import { useAtom } from 'jotai';
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 import ReactPlayer from 'react-player';
 // eslint-disable-next-line import/no-unresolved
 import BaseReactPlayer, { BaseReactPlayerProps } from 'react-player/base';
-import result2 from '../../resources/images/result/result2.png';
-import { PoseGrid } from '../utils/poseGrid';
-import { formInstructionItemsAtom, phaseAtom } from './atoms';
+import { phaseAtom } from './atoms';
+
+function FlatCard({ children }: { children: React.ReactNode }) {
+  return <Box sx={{ borderRadius: 5, p: 3, borderWidth: 6, borderColor: '#4AC0E3' }}>{children}</Box>;
+}
+
+function FlatButton({ text, onClick }: { text: string; onClick: () => void }) {
+  return (
+    <Button
+      variant="contained"
+      onClick={onClick}
+      sx={{
+        borderRadius: 20,
+        p: 2,
+        borderWidth: 6,
+        borderColor: '#4AC0E3',
+        width: '100%',
+        height: '100%',
+        fontSize: 'h5.fontSize',
+        fontWeight: 'bold',
+        paddingLeft: '70px',
+        paddingRight: '70px',
+        '&:hover': {
+          backgroundColor: '#4AC0E3',
+          color: 'white',
+        },
+      }}
+    >
+      {text}
+    </Button>
+  );
+}
 
 export default function Report2() {
   const [, setPhase] = useAtom(phaseAtom);
-  const poseGridRef = useRef<PoseGrid | null>(null);
-  const gridDivRef = useRef<HTMLDivElement | null>(null);
-  const [formInstructionItems] = useAtom(formInstructionItemsAtom);
   const videoPlayerRef = useRef<BaseReactPlayer<BaseReactPlayerProps>>(null);
 
-  // Reportコンポーネントマウント時にKinectを停止し、PoseGridを作成する
-  useEffect(() => {
-    if (!poseGridRef.current && gridDivRef.current !== null) {
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      poseGridRef.current = new PoseGrid(gridDivRef.current);
-      poseGridRef.current.setCameraAngle(formInstructionItems[0].poseGridCameraAngle);
-    }
-  }, [formInstructionItems]);
-
   return (
-    <Box display="flex" justifyContent="center">
-      <img src={result2} alt="Result2" style={{ height: '100vh' }} />
-      <ReactPlayer
-        ref={videoPlayerRef}
-        url="../../resources/images/result/sq-video.mov"
-        id="RepVideo"
-        playing
-        loop
-        controls
-        width="30vw"
-        height="31.2vw"
-        style={{
-          zIndex: 2,
-          position: 'absolute',
-          width: '420px',
-          height: '500px',
-          top: '22vh',
-          left: '12.5vw',
-          borderRadius: '24px',
-          borderColor: '#4AC0E3',
-          borderWidth: '6px',
-          backgroundColor: 'rgba(0, 0, 0, 1.0)',
-        }}
-      />
-
-      <Button
-        sx={{
-          position: 'absolute',
-          top: '89vh',
-          left: '76vw',
-          transform: 'translate(-50%, -50%)',
-          height: '60px', // 60
-          width: '225px', // 225
-          borderRadius: '40px',
-          backgroundColor: '#4AC0E3',
-          color: '#FFFFFF',
-          fontSize: '24px',
-          fontWeight: 'bold',
-          paddingLeft: '70px',
-          paddingRight: '70px',
-        }}
-        onClick={() => {
-          setPhase(0);
-        }}
-      >
-        終了
-      </Button>
-    </Box>
+    <Grid container sx={{ paddingBlock: '5vh', paddingInline: '0vw' }}>
+      <Grid item xs={12} sx={{ paddingBlock: '2.5vh', paddingInline: '5vw' }}>
+        <Typography variant="h5" component="h1" align="left" borderBottom={1} fontWeight="bold">
+          今回のトレーニング結果
+        </Typography>
+      </Grid>
+      <Grid item xs={6} sx={{ paddingBlock: '2.5vh', paddingInline: '5vw' }}>
+        <ReactPlayer
+          ref={videoPlayerRef}
+          url="../../resources/images/result/sq-video.mov"
+          id="RepVideo"
+          playing
+          loop
+          // controls
+          width="100%"
+          height="100%"
+          style={{
+            borderRadius: '24px',
+            borderColor: '#4AC0E3',
+            borderWidth: '6px',
+            backgroundColor: 'rgba(0, 0, 0, 1.0)',
+          }}
+        />
+      </Grid>
+      <Grid item xs={6} sx={{ paddingBlock: '2.5vh', paddingRight: '5vw' }}>
+        <FlatCard>
+          <Stack spacing={2} alignItems="flex-start">
+            <Chip
+              label="ポイント"
+              sx={{ fontWeight: 'bold', color: 'white', backgroundColor: '#4AC0E3', paddingInline: 1 }}
+            />
+            <Typography variant="h5" component="h1" fontWeight="bold">
+              全体的なフォームはきれいですが、 体幹に力を入れて胴体をまっすぐに保つように。 歌声にもあらわれています。
+            </Typography>
+          </Stack>
+        </FlatCard>
+        <Stack direction="row" justifyContent="space-between" alignItems="center" spacing={2} sx={{ mt: '5vh' }}>
+          <FlatCard>
+            <Stack spacing={2}>
+              <Chip
+                label="深さ"
+                sx={{ fontWeight: 'bold', color: 'white', backgroundColor: '#4AC0E3', paddingInline: 1 }}
+              />
+              <Typography variant="h5" component="h1" fontWeight="bold">
+                Good
+              </Typography>
+            </Stack>
+          </FlatCard>
+          <FlatCard>
+            <Stack spacing={2}>
+              <Chip
+                label="深さ"
+                sx={{ fontWeight: 'bold', color: 'white', backgroundColor: '#4AC0E3', paddingInline: 1 }}
+              />
+              <Typography variant="h5" component="h1" fontWeight="bold">
+                Good
+              </Typography>
+            </Stack>
+          </FlatCard>
+          <FlatCard>
+            <Stack spacing={2}>
+              <Chip
+                label="深さ"
+                sx={{ fontWeight: 'bold', color: 'white', backgroundColor: '#4AC0E3', paddingInline: 1 }}
+              />
+              <Typography variant="h5" component="h1" fontWeight="bold">
+                Good
+              </Typography>
+            </Stack>
+          </FlatCard>
+        </Stack>
+      </Grid>
+      <Grid item xs={12} sx={{ paddingBlock: '2.5vh', paddingInline: '5vw' }}>
+        <Stack direction="row" justifyContent="space-between" alignItems="center" spacing="50vw">
+          <FlatButton text="戻る" onClick={() => setPhase((prev) => prev - 1)} />
+          <FlatButton text="次へ" onClick={() => setPhase((prev) => prev + 1)} />
+        </Stack>
+      </Grid>
+    </Grid>
   );
 }
