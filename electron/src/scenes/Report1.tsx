de5f2813163397681b4ac0d8f8028b21a5412e3f
Satoshi Endo <53333254+s-endo17@users.noreply.github.com>
2022-10-17 10:55:20 +0900

    Feat/create result1 page (#145)
    * tmp commit
    
    * chore: force main window to have 16:9 aspect ratio
    
    * feat: put elements in report in place
    
    * fix: make report movie responsive
    
    * feat: create coded result1 page
    
    * change: set objectfit prop of muscle image contain
    
    Co-authored-by: Yusuke KONDO <kondounagi@gmail.com>

--- electron/src/scenes/Report1.tsx
+++ electron/src/scenes/Report1.tsx
@@ -1,10 +1,9 @@
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
 import LocalFireDepartmentIcon from '@mui/icons-material/LocalFireDepartment';
-import { Grid, Stack, Typography } from '@mui/material';
+import { CardMedia, Grid, Stack, Typography } from '@mui/material';
 import { ReactElement } from 'react';
 import result2 from '../../resources/images/chest.png';
-import { useDummySetRecordIfDebugMode } from './ui-components/SetRecordDebugger';
 
 function TextWithIcon(props: { icon: ReactElement; text: string }) {
   const { icon, text } = props;
@@ -19,8 +18,8 @@ function TextWithIcon(props: { icon: ReactElement; text: string }) {
   );
 }
 
-function ShortResult(props: { icon: ReactElement; text: string }) {
-  const { icon, text } = props;
+function ShortResult(props: { metrics: ReactElement; measuredValue: string }) {
+  const { metrics, measuredValue } = props;
 
   return (
     <Stack
@@ -31,9 +30,9 @@ function ShortResult(props: { icon: ReactElement; text: string }) {
       borderBottom={1}
       sx={{ marginInline: '5vw', mt: '5vh', p: '1vh' }}
     >
-      <TextWithIcon icon={icon} text={text} />
+      {metrics}
       <Typography variant="h5" component="h1" align="left" fontWeight="bold">
-        aaa
+        {measuredValue}
       </Typography>
     </Stack>
   );
@@ -41,7 +40,6 @@ function ShortResult(props: { icon: ReactElement; text: string }) {
 
 export default function Report1() {
   useDummySetRecordIfDebugMode();
-
   return (
     <Grid container>
       <Grid item xs={12} sx={{ mt: '7vh' }}>
@@ -54,14 +52,24 @@ export default function Report1() {
           <Typography variant="h1" component="h1" align="center" borderBottom={1} sx={{ mx: '5vw' }} fontWeight="bold">
             68
           </Typography>
-          <ShortResult text="時間" icon={<AccessTimeIcon fontSize="large" color="primary" />} />
-          <ShortResult text="回数" icon={<FitnessCenterIcon fontSize="large" color="primary" />} />
-          <ShortResult text="消費カロリー" icon={<LocalFireDepartmentIcon fontSize="large" color="primary" />} />
+          <ShortResult
+            metrics={<TextWithIcon icon={<AccessTimeIcon fontSize="large" color="primary" />} text="時間" />}
+            measuredValue="55:35"
+          />
+          <ShortResult
+            metrics={<TextWithIcon icon={<FitnessCenterIcon fontSize="large" color="primary" />} text="回数" />}
+            measuredValue="5回"
+          />
+          <ShortResult
+            metrics={
+              <TextWithIcon icon={<LocalFireDepartmentIcon fontSize="large" color="primary" />} text="消費カロリー" />
+            }
+            measuredValue="92kcal"
+          />
         </Stack>
       </Grid>
       <Grid item xs={6} sx={{ mt: '7vh' }} alignContent="center" justifyContent="center">
-        <img src={result2} alt="Result2" style={{ height: '50vh' }} />
-        {/* <CardMedia component="img" image={result2} alt="Result2" style={{ height: '50vh' }} /> */}
+        <CardMedia component="img" image={result2} alt="Result2" style={{ height: '50vh', objectFit: 'contain' }} />
       </Grid>
     </Grid>
   );
