d850aa2b236a66fff8be5f111889aab4aa5eae37
Yusuke KONDO <kondounagi@gmail.com>
2022-11-20 01:51:29 +0900

    feat: concat videos from 4 cams to one video

--- python/multiple_cam_manager/main.py
+++ python/multiple_cam_manager/main.py
@@ -2,6 +2,7 @@ import argparse
 import logging
 from datetime import datetime
 from pathlib import Path
+import time
 
 import cv2
 import numpy as np
@@ -13,14 +14,20 @@ OUTPUT_DIR = "output"
 def ger_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--num-camera", type=int, help="Number of camera", default=2, required=False
+        "--cam-ids",
+        "-i",
+        type=int,
+        nargs="+",
+        help="Camera ids",
+        default=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+        required=False,
     )
     return parser.parse_args()
 
 
-def init_cams(num_camera: int):
+def init_cams(cam_ids: list[int]):
     caps = {}
-    for i in range(num_camera):
+    for i in cam_ids:
         cap = cv2.VideoCapture(i)
         if not cap.isOpened():
             logging.error("Camera {} is not available".format(i))
@@ -31,7 +38,7 @@ def init_cams(num_camera: int):
 def init_video_writer(
     path_to_save: str | None,
     size: tuple[int, int],
-    fps: int = 30,
+    fps: int = 10,
 ):
     if path_to_save is None:
         now = datetime.now().strftime("%Y%m%d_%H%M%S")
@@ -49,7 +56,9 @@ def init_video_writer(
 
 def main():
     args = ger_args()
-    caps = init_cams(args.num_camera)
+    cam_ids = args.cam_ids
+    num_cameras = len(cam_ids)
+    caps = init_cams(cam_ids)
     video_writer = None
 
     while True:
@@ -61,7 +70,6 @@ def main():
             is_avails.append(is_avail)
 
         resize_shape = (frames[0].shape[1], frames[0].shape[0])
-        # resize_shape = (100, 100)
         resized_frames = [
             cv2.resize(frame, resize_shape)
             for frame, is_avail in zip(frames, is_avails)
@@ -70,20 +78,26 @@ def main():
 
         # ---- Option 1 ----
         # numpy_vertical = np.vstack((frame1, frame2))
-        numpy_horizontal = np.hstack((resized_frames[0], resized_frames[1]))
+        num_avail_cam = sum(is_avails)
+        result = np.vstack(
+            (
+                np.hstack((resized_frames[0], resized_frames[1])),
+                np.hstack((resized_frames[2], resized_frames[3])),
+            )
+        )
 
         # ---- Option 2 ----
         # numpy_vertical_concat = np.concatenate((image, grey_3_channel), axis=0)
         # numpy_horizontal_concat = np.concatenate((frame1, frame2), axis=1)
 
         if video_writer is None:
-            concatted_shape = (numpy_horizontal.shape[1], numpy_horizontal.shape[0])
+            concatted_shape = (result.shape[1], result.shape[0])
             video_writer = init_video_writer(path_to_save=None, size=concatted_shape)
 
-        video_writer.write(numpy_horizontal)
+        video_writer.write(result)
 
-        cv2.imshow("Result", numpy_horizontal)
-        key_input = cv2.waitKey(20)
+        cv2.imshow("Result", result)
+        key_input = cv2.waitKey(1)
         if key_input == 27:  # exit on ESC
             video_writer.release()
             for cap in caps.values():
