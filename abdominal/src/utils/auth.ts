d1f4fff0f1468819b88e6f66aafe549125cd9761
Yusuke KONDO <kondounagi@gmail.com>
2022-12-25 15:04:54 +0900

    feat: add utils to sign-in and sign-out using amplify #195

---
+++ abdominal/src/utils/auth.ts
@@ -0,0 +1,83 @@
+import { CognitoUser } from '@aws-amplify/auth';
+import { Amplify, Auth } from 'aws-amplify';
+
+Amplify.configure({
+  Auth: {
+    // REQUIRED only for Federated Authentication - Amazon Cognito Identity Pool ID
+    identityPoolId: 'us-west-2:47bf8c84-9a1f-41f2-a8dc-ad2f9d68b96d',
+
+    // REQUIRED - Amazon Cognito Region
+    region: 'us-west-2',
+
+    // OPTIONAL - Amazon Cognito Federated Identity Pool Region
+    // Required only if it's different from Amazon Cognito Region
+    // identityPoolRegion: 'XX-XXXX-X',
+
+    // OPTIONAL - Amazon Cognito User Pool ID
+    userPoolId: 'us-west-2_d0FYMjTcW',
+
+    // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+    // userPoolWebClientId: 'a1b2c3d4e5f6g7h8i9j0k1l2m3',
+
+    // OPTIONAL - Enforce user authentication prior to accessing AWS resources or not
+    mandatorySignIn: false,
+
+    // OPTIONAL - This is used when autoSignIn is enabled for Auth.signUp
+    // 'code' is used for Auth.confirmSignUp, 'link' is used for email link verification
+    signUpVerificationMethod: 'link', // 'code' | 'link'
+
+    // OPTIONAL - Configuration for cookie storage
+    // Note: if the secure flag is set to true, then the cookie transmission requires a secure protocol
+    // cookieStorage: {
+    //   // REQUIRED - Cookie domain (only required if cookieStorage is provided)
+    //   domain: '.yourdomain.com',
+    //   // OPTIONAL - Cookie path
+    //   path: '/',
+    //   // OPTIONAL - Cookie expiration in days
+    //   expires: 365,
+    //   // OPTIONAL - See: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie/SameSite
+    //   sameSite: 'strict' | 'lax',
+    //   // OPTIONAL - Cookie secure flag
+    //   // Either true or false, indicating if the cookie transmission requires a secure protocol (https).
+    //   secure: true,
+    // },
+
+    // OPTIONAL - customized storage object
+    // storage: MyStorage,
+
+    // OPTIONAL - Manually set the authentication flow type. Default is 'USER_SRP_AUTH'
+    // authenticationFlowType: 'USER_PASSWORD_AUTH',
+
+    // OPTIONAL - Manually set key value pairs that can be passed to Cognito Lambda Triggers
+    // clientMetadata: { myCustomKey: 'myCustomValue' },
+
+    // OPTIONAL - Hosted UI configuration
+    oauth: {
+      domain: 'https://abdominal-user-pool-development.auth.us-west-2.amazoncognito.com',
+      scope: ['email', 'openid', 'phone'], // ['phone', 'email', 'profile', 'openid', 'aws.cognito.signin.user.admin'],
+      redirectSignIn: 'http://localhost:7777/',
+      redirectSignOut: 'http://localhost:7777/',
+      responseType: 'code', // or 'token', note that REFRESH token will only be generated when the responseType is code
+    },
+  },
+});
+
+export async function signIn(username: string, password: string): Promise<typeof CognitoUser | false> {
+  try {
+    const user = (await Auth.signIn(username, password)) as typeof CognitoUser;
+
+    return user;
+  } catch (e) {
+    window.log.error(e);
+
+    return false;
+  }
+}
+
+export async function signOut(): Promise<void> {
+  try {
+    await Auth.signOut();
+  } catch (e) {
+    window.log.error(e);
+  }
+}
