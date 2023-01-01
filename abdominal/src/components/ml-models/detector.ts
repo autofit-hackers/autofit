c4f044cf9784b3141c8c67931975aa2a6c6ea46d
masaueno <ueno-masaki202@g.ecc.u-tokyo.ac.jp>
2023-01-01 21:23:34 +0900

    refactor: rename file

--- abdominal/src/components/ml-models/detector.ts
+++ abdominal/src/components/ml-models/detector.ts
@@ -8,24 +8,134 @@ export type DetectionResults = {
   classesData: Float32Array | Int32Array | Uint8Array;
 };
 
-export type DetectionModel = {
-  net: tf.GraphModel<string | tf.io.IOHandler>;
+export type DetectorOptions = {
+  threshold: number;
+  setWeight: (weight: number) => void;
+  setPlates: (plates: string[]) => void;
+  setIsExercising: (doingExercise: boolean) => void;
+};
+
+export type Detector = {
+  model: tf.GraphModel<string | tf.io.IOHandler>;
   inputWidth: number;
   inputHeight: number;
+  options: DetectorOptions;
 };
 
 export type LoadingProps = { loading: boolean; progress: number };
 
-export type BarbellDetector = {
-  model: DetectionModel;
-  threshold: number;
-  setWeight: (weight: number) => void;
-  setPlates: (plates: string[]) => void;
-  setDoingExercise: (doingExercise: boolean) => void;
+export const getTotalWeight = ({ boxesData, scoresData, classesData }: DetectionResults, threshold: number) => {
+  let weight = 0;
+  const plates = [];
+  let barbellCenterZ = 0;
+  for (let i = 0; i < scoresData.length; i += 1) {
+    if (scoresData[i] > threshold) {
+      const klass = labels[classesData[i]];
+      if (klass === '20kg-bar') {
+        weight += 20;
+        plates.push('バーベル');
+        barbellCenterZ = (boxesData[i * 4] + boxesData[i * 4 + 2]) / 2;
+      } else if (klass === '10kg-plate') {
+        weight += 20;
+        plates.push('10kg');
+      } else if (klass === '5kg-plate') {
+        weight += 10;
+        plates.push('5kg');
+      } else if (klass === '2.5kg-plate') {
+        weight += 5;
+        plates.push('2.5kg');
+      } else {
+        console.warn(`Unknown class: ${klass}`);
+      }
+    }
+  }
+
+  return { weight, plates, barbellCenterZ };
+};
+
+export const loadDetectionModel = (
+  modelName: string,
+  options: DetectorOptions,
+  setLoading: Dispatch<
+    SetStateAction<{
+      loading: boolean;
+      progress: number;
+    }>
+  >,
+): Detector | void => {
+  tf.ready()
+    .then(async () => {
+      // load model
+      const model = await tf.loadGraphModel(`${window.location.origin}/${modelName}_web_model/model.json`, {
+        onProgress: (fractions) => {
+          setLoading({ loading: true, progress: fractions }); // set loading fractions
+        },
+      });
+
+      if (model.inputs[0].shape == null) {
+        throw new Error('Invalid model input shape');
+      }
+      // warming up model
+      const dummyInput = tf.ones(model.inputs[0].shape);
+      const warmupResult = await model.executeAsync(dummyInput);
+      tf.dispose(warmupResult); // cleanup memory
+      tf.dispose(dummyInput); // cleanup memory
+
+      setLoading({ loading: false, progress: 1 });
+
+      return {
+        model,
+        inputWidth: model.inputs[0].shape[0],
+        inputHeight: model.inputs[0].shape[3],
+        options,
+      };
+    })
+    .catch((err) => {
+      throw err;
+    });
+};
+
+export const detectObjectsOnFrame = async (detector: Detector | undefined, cameraCanvas: HTMLCanvasElement) => {
+  if (cameraCanvas === null) return;
+  if (!detector) throw new Error('Model is null or undefined');
+
+  tf.engine().startScope();
+  const input = tf.tidy(() =>
+    tf.image
+      .resizeBilinear(tf.browser.fromPixels(cameraCanvas), [detector.inputWidth, detector.inputHeight])
+      .div(255.0)
+      .expandDims(0),
+  );
+
+  const { threshold } = detector.options;
+  await detector.model.executeAsync(input).then((result) => {
+    if (!Array.isArray(result)) throw new Error('Model output is not an array');
+    const [boxes, scores, classes] = result.slice(0, 3);
+    const boxesData = boxes.dataSync();
+    const scoresData = scores.dataSync();
+    const classesData = classes.dataSync();
+    const { weight, plates, barbellCenterZ } = getTotalWeight(
+      {
+        boxesData,
+        scoresData,
+        classesData,
+      },
+      threshold,
+    );
+    if (barbellCenterZ !== 0) {
+      detector.options.setWeight(weight);
+      detector.options.setPlates(plates);
+      detector.options.setIsExercising(barbellCenterZ < 0.5);
+    }
+    tf.dispose(result);
+  });
+
+  tf.engine().endScope();
 };
 
 export const renderBoxes = (
   canvas: HTMLCanvasElement,
+  videoCanvas: HTMLCanvasElement,
   ConfidenceThre: number,
   boxesData: Float32Array | Int32Array | Uint8Array | undefined,
   scoresData: Float32Array | Int32Array | Uint8Array | undefined,
@@ -78,111 +188,4 @@ export const renderBoxes = (
       ctx.fillText(`${label} - ${score}%`, x1 - 1, yText < 0 ? 0 : yText);
     }
   }
-};
-
-export const getTotalWeight = ({ boxesData, scoresData, classesData }: DetectionResults, threshold: number) => {
-  let weight = 0;
-  const plates = [];
-  let barbellCenterZ = 0;
-  for (let i = 0; i < scoresData.length; i += 1) {
-    if (scoresData[i] > threshold) {
-      const klass = labels[classesData[i]];
-      if (klass === '20kg-bar') {
-        weight += 20;
-        plates.push('バーベル');
-        barbellCenterZ = (boxesData[i * 4] + boxesData[i * 4 + 2]) / 2;
-      } else if (klass === '10kg-plate') {
-        weight += 20;
-        plates.push('10kg');
-      } else if (klass === '5kg-plate') {
-        weight += 10;
-        plates.push('5kg');
-      } else if (klass === '2.5kg-plate') {
-        weight += 5;
-        plates.push('2.5kg');
-      } else {
-        console.warn(`Unknown class: ${klass}`);
-      }
-    }
-  }
-
-  return { weight, plates, barbellCenterZ };
-};
-
-export const loadDetectionModel = (
-  modelName: string,
-  setLoading: Dispatch<
-    SetStateAction<{
-      loading: boolean;
-      progress: number;
-    }>
-  >,
-): DetectionModel | void => {
-  tf.ready()
-    .then(async () => {
-      // load model
-      const model = await tf.loadGraphModel(`${window.location.origin}/${modelName}_web_model/model.json`, {
-        onProgress: (fractions) => {
-          setLoading({ loading: true, progress: fractions }); // set loading fractions
-        },
-      });
-
-      if (model.inputs[0].shape == null) {
-        throw new Error('Invalid model input shape');
-      }
-      // warming up model
-      const dummyInput = tf.ones(model.inputs[0].shape);
-      const warmupResult = await model.executeAsync(dummyInput);
-      tf.dispose(warmupResult); // cleanup memory
-      tf.dispose(dummyInput); // cleanup memory
-
-      setLoading({ loading: false, progress: 1 });
-
-      return {
-        net: model,
-        inputWidth: model.inputs[0].shape[0],
-        inputHeight: model.inputs[0].shape[3],
-      };
-    })
-    .catch((err) => {
-      throw err;
-    });
-};
-
-export const detectObjectsOnFrame = async (detector: BarbellDetector, cameraCanvas: HTMLCanvasElement) => {
-  if (cameraCanvas === null) return;
-  if (detector.model.net == null) throw new Error('Model is null or undefined');
-
-  tf.engine().startScope();
-  const input = tf.tidy(() =>
-    tf.image
-      .resizeBilinear(tf.browser.fromPixels(cameraCanvas), [detector.model.inputWidth, detector.model.inputHeight])
-      .div(255.0)
-      .expandDims(0),
-  );
-
-  const { threshold } = detector;
-  await detector.model.net.executeAsync(input).then((result) => {
-    if (!Array.isArray(result)) throw new Error('Model output is not an array');
-    const [boxes, scores, classes] = result.slice(0, 3);
-    const boxesData = boxes.dataSync();
-    const scoresData = scores.dataSync();
-    const classesData = classes.dataSync();
-    const { weight, plates, barbellCenterZ } = getTotalWeight(
-      {
-        boxesData,
-        scoresData,
-        classesData,
-      },
-      threshold,
-    );
-    if (barbellCenterZ !== 0) {
-      detector.setWeight(weight);
-      detector.setPlates(plates);
-      detector.setDoingExercise(barbellCenterZ < 0.5);
-    }
-    tf.dispose(result);
-  });
-
-  tf.engine().endScope();
 };
