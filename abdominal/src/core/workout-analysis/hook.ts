4cd0427fa3dfd10a5a58e81f6da04d3c547500a7
masaueno <ueno-masaki202@g.ecc.u-tokyo.ac.jp>
2023-01-07 13:52:13 +0900

    refactor: translate type of pose estimation results in pose hooks

--- abdominal/src/core/workout-analysis/hook.ts
+++ abdominal/src/core/workout-analysis/hook.ts
@@ -1,7 +1,7 @@
-import { Options, Results } from '@mediapipe/pose';
+import { Options } from '@mediapipe/pose';
 import { useCallback, RefObject, useRef } from 'react';
 import { updateWorkoutState, WorkoutState } from 'src/core/workout-analysis/workoutState';
-import { Pose, rotateWorldLandmarks } from 'src/library/pose-estimation/pose';
+import { Pose } from 'src/library/pose-estimation/pose';
 import { updateRepCountState, getDefaultRepCountState } from 'src/core/workout-analysis/modules/countReps';
 import { updateExerciseEstimationState } from 'src/core/workout-analysis/modules/identifyExercise';
 import drawPose from 'src/library/pose-estimation/drawPose';
@@ -25,14 +25,7 @@ const useWorkoutAnalysis = (
     minTrackingConfidence: 0.8,
   };
   const onResults = useCallback(
-    (results: Results) => {
-      if (!('poseLandmarks' in results)) return;
-      const pose: Pose = {
-        imageLandmarks: results.poseLandmarks,
-        worldLandmarks: rotateWorldLandmarks(results.poseWorldLandmarks, { roll: 180, pitch: 0, yaw: 0 }),
-        timestamp: new Date().getTime(),
-      };
-      // TODO: 外れ値処理
+    (pose: Pose) => {
       drawPose(canvasRef.current, pose, 'limegreen');
       exerciseEstimationState.current = updateExerciseEstimationState(exerciseEstimationState.current, pose);
       repCountState.current = updateRepCountState(repCountState.current, pose);
--- abdominal/src/library/pose-estimation/hook.ts
+++ abdominal/src/library/pose-estimation/hook.ts
@@ -1,10 +1,11 @@
 import { Options, Results, Pose as PoseMediapipe } from '@mediapipe/pose';
 import { useCallback, useEffect, useState } from 'react';
+import { Pose, rotateWorldLandmarks } from 'src/library/pose-estimation/pose';
 import { loadPoseEstimator, sendFrameToPoseEstimator } from 'src/library/pose-estimation/poseEstimator';
 
 export type PoseEstimationConfig = {
   options: Options;
-  onResults: (results: Results) => void;
+  onResults: (pose: Pose) => void;
 };
 
 const usePoseEstimation = ({
@@ -16,11 +17,25 @@ const usePoseEstimation = ({
     async (canvas: HTMLCanvasElement) => sendFrameToPoseEstimator(poseEstimator, canvas),
     [poseEstimator],
   );
+  const onPoseResult = useCallback(
+    (results: Results) => {
+      if (!('poseLandmarks' in results)) return;
+      // TODO: ランドマークの回転を調整する
+      const pose: Pose = {
+        imageLandmarks: results.poseLandmarks,
+        worldLandmarks: rotateWorldLandmarks(results.poseWorldLandmarks, { roll: 180, pitch: 0, yaw: 0 }),
+        timestamp: new Date().getTime(),
+      };
+      // TODO: 外れ値処理
+      onResults(pose);
+    },
+    [onResults],
+  );
 
   // Activate ML models
   useEffect(() => {
-    setPoseEstimator(loadPoseEstimator(options, onResults));
-  }, [onResults, options]);
+    setPoseEstimator(loadPoseEstimator(options, onPoseResult));
+  }, [onPoseResult, options]);
 
   return estimatePose;
 };
